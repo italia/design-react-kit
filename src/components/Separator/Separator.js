f06cd2af0e520425d7b0bd9cbda7d35464ac2fca
dej611 <dej611@gmail.com>
2017-10-07 15:14:04 +0200

    :sparkles: Add Separator

---
+++ src/components/Separator/Separator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Separator = () => {
+  return (
+    <div>
+      <hr className="Separator-room u-background-50" />
+      <hr className="Separator Separator--up u-background-grey-20" />
+    </div>
+  );
+};
+
+export default Separator;
