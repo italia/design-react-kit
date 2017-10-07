a9040f9730c21c406d28f076ac3b9f2d2274f9bd
Matteo Manchi <matteo.manchi@gmail.com>
2017-10-07 20:00:07 +0200

    eslint fix

--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -9,24 +9,38 @@ storiesOf('Button', module)
         'Default Button',
         withInfo('Default button')(() => (
             <div>
-                <Button type="default">Button default</Button><br /><br />
-                <Button disabled>Button default disabled</Button><br /><br />
-                <Button shadow>Button default shadow</Button><br /><br />
-                <Button round>Button default round</Button><br /><br />
-                <Button shadow round>Button default round shadow</Button><br /><br />
-                <Button shadow round link>LinkButton default round shadow</Button>
+                <Button type="default">Button default</Button>
+                <br />
+                <br />
+                <Button disabled>Button default disabled</Button>
+                <br />
+                <br />
+                <Button shadow>Button default shadow</Button>
+                <br />
+                <br />
+                <Button round>Button default round</Button>
+                <br />
+                <br />
+                <Button shadow round>
+                    Button default round shadow
+                </Button>
+                <br />
+                <br />
+                <Button shadow round link>
+                    LinkButton default round shadow
+                </Button>
             </div>
-        ))
+        )),
     )
     .add(
         'Info Button',
         withInfo('Info button')(() => (
-            <Button type='info' >I am a button</Button>
-        ))
+            <Button type="info">I am a button</Button>
+        )),
     )
     .add(
         'Danger Button',
         withInfo('Danger button')(() => (
-            <Button type='danger' >I am a button</Button>
-        ))
+            <Button type="danger">I am a button</Button>
+        )),
     );
