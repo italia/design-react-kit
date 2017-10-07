39956660174bd79e64e92bbd9f0a0ec17e6f061b
Matteo Manchi <matteo.manchi@gmail.com>
2017-10-07 17:02:19 +0200

    eslint fix

--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NavItem = ({ children, onClick, ...rest }) => {
-  return (
-    <li className="u-padding-right-l">
-      <a onClick={onClick} {...rest}>
-        {children}
-      </a>
-    </li>
-  );
+const NavItem = ({children, onClick, ...rest}) => {
+    return (
+        <li className="u-padding-right-l">
+            <a onClick={onClick} {...rest}>
+                {children}
+            </a>
+        </li>
+    );
 };
 
 NavItem.propTypes = {
-  onClick: PropTypes.func,
-  children: PropTypes.node,
+    onClick: PropTypes.func,
+    children: PropTypes.node,
 };
 
 NavItem.defaultProps = {
-  href: '#',
+    href: '#',
 };
 
 export default NavItem;
