d7a29e705bf7fe80858d30376e6e1d6f2ba14f81
Paolo Mariotti <paolo.mariotti@sourcesense.com>
2017-10-07 15:44:20 +0200

    button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-// import 'design-web-toolkit/build/build.css';
+const Button = ({style, shadow, round, children, link, ...rest}) => {
+    const classes = classnames({
+        Button: true,
+        'Button--default': !style || style === 'default',
+        'Button--info': style === 'info',
+        'Button--danger': style === 'danger',
+        'Button--round': round,
+        'Button--shadow': shadow,
+        'u-text-r-xs': true,
+    });
 
-const Button = ({children, ...rest}) => {
-    return (<button className="Button" {...rest}>{children}</button>);
+    let ButtonTag = link ? 'a' : 'button';
+
+    // let Element = props => <button type="button" {...props} />;
+    // if (link) {
+    //     Element = function elementA(props) {
+    //         return <a role="button" {...props} />;
+    //     };
+    // }
+    return (
+        <ButtonTag
+            type={!link ? 'button' : undefined}
+            role={link ? 'button' : undefined}
+            className={classes}
+            {...rest}>
+            {children}
+        </ButtonTag>
+    );
+};
+
+Button.defaultProps = {
+    style: 'default',
+    shadow: false,
+    round: false,
+    link: false,
 };
 
 Button.propTypes = {
-    type: PropTypes.oneOf(['info', 'danger']),
+    style: PropTypes.oneOf(['info', 'danger', 'default']),
+    shadow: PropTypes.bool,
+    round: PropTypes.bool,
+    link: PropTypes.bool,
     children: PropTypes.node,
 };
 
--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -4,9 +4,29 @@ import {action} from '@storybook/addon-actions';
 import {withInfo} from '@storybook/addon-info';
 import Button from './Button';
 
-storiesOf('Button', module).add(
-    'Button',
-    withInfo('Simple button')(() => (
-        <Button onClick={action('clicked')}>I am a button</Button>
-    )),
-);
+storiesOf('Button', module)
+    .add(
+        'Default Button',
+        withInfo('Default button')(() => (
+            <div>
+                <Button type="default">Button default</Button><br /><br />
+                <Button disabled>Button default disabled</Button><br /><br />
+                <Button shadow>Button default shadow</Button><br /><br />
+                <Button round>Button default round</Button><br /><br />
+                <Button shadow round>Button default round shadow</Button><br /><br />
+                <Button shadow round link>LinkButton default round shadow</Button>
+            </div>
+        ))
+    )
+    .add(
+        'Info Button',
+        withInfo('Info button')(() => (
+            <Button type='info' >I am a button</Button>
+        ))
+    )
+    .add(
+        'Danger Button',
+        withInfo('Danger button')(() => (
+            <Button type='danger' >I am a button</Button>
+        ))
+    );
