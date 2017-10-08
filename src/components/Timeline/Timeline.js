5a87b9103633ed05a507c1232d4c6dd83796174b
Paolo Mariotti <paolo.mariotti@sourcesense.com>
2017-10-08 04:05:42 +0200

    rimosso import non più utilizzato

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import TimelineContent from './TimelineContent';
-
 const Timeline = ({children, title, background, color}) => {
     const pointClassname = classnames(
         'Timeline-point-content',
