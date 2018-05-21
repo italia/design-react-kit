c908836165e7c6ba6b8ee8c32445e9db6f0fe9e2
Gianluca Esposito <g@esposi.to>
2018-05-21 12:17:10 +0200

    Update Breadcrumb stories

--- stories/Breadcrumb/Breadcrumb.stories.js
+++ stories/Breadcrumb/Breadcrumb.stories.js
@@ -1,35 +1,149 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import { withDocs } from "../utils";
+import {storiesOf} from "@storybook/react";
+import {withDocs} from "../utils";
 
-import { Breadcrumb, BreadcrumbItem } from "../../src";
+import {Breadcrumb, BreadcrumbItem} from "../../src";
 
 import Esempi from "./Esempi.md";
+import Icone from "./Icone.md";
+import Background from "./Background.md";
 
 const stories = storiesOf("Componenti/Breadcrumb", module);
 
 stories.add(
-  "Esempi",
-  withDocs(Esempi, () => (
-    <div>
-      <Breadcrumb>
-        <BreadcrumbItem active>Home</BreadcrumbItem>
-      </Breadcrumb>
-      <Breadcrumb>
-        <BreadcrumbItem>
-          <a href="#">Home</a>
-        </BreadcrumbItem>
-        <BreadcrumbItem active>Library</BreadcrumbItem>
-      </Breadcrumb>
-      <Breadcrumb>
-        <BreadcrumbItem>
-          <a href="#">Home</a>
-        </BreadcrumbItem>
-        <BreadcrumbItem>
-          <a href="#">Library</a>
-        </BreadcrumbItem>
-        <BreadcrumbItem active>Data</BreadcrumbItem>
-      </Breadcrumb>
-    </div>
-  ))
+    "Esempi",
+    withDocs(Esempi, () => (
+        <section>
+            <nav className="breadcrumb-container">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Home<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Subsection<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+
+            <nav className="breadcrumb-container">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Home<span className="separator">&gt;</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Subsection<span className="separator">&gt;</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+        </section>
+    ))
+);
+
+stories.add(
+    "Con icone",
+    withDocs(Icone, () => (
+        <section>
+            <nav className="breadcrumb-container">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Home<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Subsection<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <i className="it-favorite" />
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+        </section>
+    ))
+);
+
+stories.add(
+    "Su sfondo scuro",
+    withDocs(Background, () => (
+        <section>
+            <nav className="breadcrumb-container">
+                <Breadcrumb className="dark">
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Home<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <a href="#">
+                            Subsection<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+
+            <nav className="breadcrumb-container">
+                <Breadcrumb className="dark">
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Home<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Subsection<span className="separator">/</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <i className="it-favorite" />
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+
+            <nav className="breadcrumb-container">
+                <Breadcrumb className="dark">
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Home<span className="separator">&gt;</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <i className="it-favorite" />
+                        <a href="#">
+                            Subsection<span className="separator">&gt;</span>
+                        </a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        <i className="it-favorite" />
+                        <a href="#">Current section</a>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </nav>
+        </section>
+    ))
 );
---
+++ stories/Breadcrumb/Icone.md
@@ -0,0 +1,5 @@
+### Con icone
+
+Per aggiungere un icona all’elemento breadcrumb è sufficiente inserire il tag `<i className="(classeicona)">` prima del tag `<a>` con la classe dell’icona che si intende visualizzare.
+
+<!-- STORY -->
