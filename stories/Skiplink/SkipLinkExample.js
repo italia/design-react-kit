d5fcfd1171656478dc3c1a8546db8e1fbd78f319
Gianluca Esposito <g@esposi.to>
2018-05-24 15:31:27 +0200

    Add SkipLink stories

---
+++ stories/Skiplink/SkipLinkExample.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Skiplink, SkiplinkItem } from "../../src";
+
+class SkipLinkExample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.container = React.createRef();
+    }
+
+    componentDidMount() {
+        // We focus the container here since it is hosted inside Storybook's iframe
+        const container = this.container.current;
+        container.focus();
+    }
+
+    render() {
+        return (
+            <section tabIndex="-1" ref={this.container}>
+                <div id="#main" />
+                <div id="#footer" />
+
+                <Skiplink>
+                    <SkiplinkItem />
+                    <SkiplinkItem href="#main">
+                        Skip to main content
+                    </SkiplinkItem>
+                    <SkiplinkItem href="#footer">Skip to footer</SkiplinkItem>
+                </Skiplink>
+            </section>
+        );
+    }
+}
+
+export default SkipLinkExample;
