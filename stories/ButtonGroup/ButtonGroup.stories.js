4ec97b083b3d60e3edc19ded80bc9643eea12f8e
Gianluca Esposito <g@esposi.to>
2018-05-24 10:46:40 +0200

    Disable unused story

--- stories/ButtonGroup/ButtonGroup.stories.js
+++ stories/ButtonGroup/ButtonGroup.stories.js
@@ -12,84 +12,94 @@ import Dimensioni from "./Dimensioni.md";
 import Vertical from "./Vertical.md";
 import Nesting from "./Nesting.md";
 
-const stories = storiesOf("Componenti/Button Group", module);
+// const stories = storiesOf("Componenti/Button Group", module);
+const stories = {
+    add: story => {
+        console.info(`'${story}' story has been disabled`);
+    }
+};
 
 stories.add(
-  "Esempi",
-  withDocs(Esempi, () => (
-    <ButtonGroup>
-      <Button>Left</Button>
-      <Button>Middle</Button>
-      <Button>Right</Button>
-    </ButtonGroup>
-  ))
-);
-stories.add(
-  "Toolbar",
-  withDocs(Toolbar, () => (
-    <ButtonToolbar>
-      <ButtonGroup className="mr-1">
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>4</Button>
-      </ButtonGroup>
-      <ButtonGroup className="mr-1">
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button>7</Button>
-      </ButtonGroup>
-      <ButtonGroup className="mr-1">
-        <Button>8</Button>
-      </ButtonGroup>
-    </ButtonToolbar>
-  ))
-);
-stories.add(
-  "Dimensioni",
-  withDocs(Dimensioni, () => (
-    <section>
-      <div>
-        <ButtonGroup size="lg">
-          <Button>Left</Button>
-          <Button>Middle</Button>
-          <Button>Right</Button>
-        </ButtonGroup>
-      </div>
-      <div className="mt-3">
+    "Esempi",
+    withDocs(Esempi, () => (
         <ButtonGroup>
-          <Button>Left</Button>
-          <Button>Middle</Button>
-          <Button>Right</Button>
+            <Button>Left</Button>
+            <Button>Middle</Button>
+            <Button>Right</Button>
         </ButtonGroup>
-      </div>
-      <div className="mt-3">
-        <ButtonGroup size="sm">
-          <Button>Left</Button>
-          <Button>Middle</Button>
-          <Button>Right</Button>
-        </ButtonGroup>
-      </div>
-    </section>
-  ))
+    ))
 );
 stories.add(
-  "Vertical",
-  withDocs(Vertical, () => (
-    <ButtonGroup vertical>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Button>Button</Button>
-    </ButtonGroup>
-  ))
+    "Toolbar",
+    withDocs(Toolbar, () => (
+        <ButtonToolbar>
+            <ButtonGroup className="mr-1">
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+                <Button>4</Button>
+            </ButtonGroup>
+            <ButtonGroup className="mr-1">
+                <Button>5</Button>
+                <Button>6</Button>
+                <Button>7</Button>
+            </ButtonGroup>
+            <ButtonGroup className="mr-1">
+                <Button>8</Button>
+            </ButtonGroup>
+        </ButtonToolbar>
+    ))
+);
+stories.add(
+    "Dimensioni",
+    withDocs(Dimensioni, () => (
+        <section>
+            <div>
+                <ButtonGroup size="lg">
+                    <Button>Left</Button>
+                    <Button>Middle</Button>
+                    <Button>Right</Button>
+                </ButtonGroup>
+            </div>
+            <div className="mt-3">
+                <ButtonGroup>
+                    <Button>Left</Button>
+                    <Button>Middle</Button>
+                    <Button>Right</Button>
+                </ButtonGroup>
+            </div>
+            <div className="mt-3">
+                <ButtonGroup size="sm">
+                    <Button>Left</Button>
+                    <Button>Middle</Button>
+                    <Button>Right</Button>
+                </ButtonGroup>
+            </div>
+        </section>
+    ))
+);
+stories.add(
+    "Vertical",
+    withDocs(Vertical, () => (
+        <ButtonGroup vertical>
+            <Button>Button</Button>
+            <Button>Button</Button>
+            <Button>Button</Button>
+            <Button>Button</Button>
+            <Button>Button</Button>
+            <Button>Button</Button>
+        </ButtonGroup>
+    ))
 );
 
-const moreStories = storiesOf("Componenti/Button Group/Nesting", module);
+// const moreStories = storiesOf("Componenti/Button Group/Nesting", module);
+const moreStories = {
+    add: story => {
+        console.info(`'${story}' story has been disabled`);
+    }
+};
 
 moreStories.add(
-  "Esempi",
-  withDocs(Nesting, () => <ButtonGroupDropdownExample />)
+    "Esempi",
+    withDocs(Nesting, () => <ButtonGroupDropdownExample />)
 );
