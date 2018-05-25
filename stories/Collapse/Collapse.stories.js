6c3f558ca74b144573a7e24a9433d590e91fd40d
Gianluca Esposito <g@esposi.to>
2018-05-25 17:49:44 +0200

    Update Collapse stories

--- stories/Collapse/Collapse.stories.js
+++ stories/Collapse/Collapse.stories.js
@@ -3,15 +3,27 @@ import { storiesOf } from "@storybook/react";
 import { withDocs } from "../utils";
 
 import CollapseExample from "./CollapseExample";
+import CollapseGroupsExample from "./CollapseGroupsExample";
 import CollapseAccordionExample from "./CollapseAccordionExample";
+import CollapseNestedExample from "./CollapseNestedExample";
 
 import Esempi from "./Esempi.md";
+import Gruppi from "./Gruppi.md";
 import Accordion from "./Accordion.md";
+import Innestati from "./Innestati.md";
 
 const stories = storiesOf("Componenti/Collapse", module);
 
 stories.add("Esempi", withDocs(Esempi, () => <CollapseExample />));
 stories.add(
-  "Accordion",
-  withDocs(Accordion, () => <CollapseAccordionExample />)
+    "Gruppi di elementi richiudibili",
+    withDocs(Gruppi, () => <CollapseGroupsExample />)
+);
+stories.add(
+    "Accordion",
+    withDocs(Accordion, () => <CollapseAccordionExample />)
+);
+stories.add(
+    "Collapse e Accordion innestati",
+    withDocs(Innestati, () => <CollapseNestedExample />)
 );
---
+++ stories/Collapse/Innestati.md
@@ -0,0 +1,5 @@
+#### Collapse e Accordion innestati
+
+Più gruppi di collapse possono essere annidati.
+
+<!-- STORY -->
