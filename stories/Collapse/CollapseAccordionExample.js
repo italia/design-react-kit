547cb1334837f4053de68c236e43d2f3c444e785
Gianluca Esposito <g@esposi.to>
2018-05-28 12:34:33 +0200

    Update Accordion stories

--- stories/Collapse/CollapseAccordionExample.js
+++ stories/Collapse/CollapseAccordionExample.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Collapse, Card, CardBody } from "../../src";
+import { Accordion, AccordionHeader, AccordionBody } from "../../src";
 
 class CollapseAccordionExample extends React.Component {
     defaultState = {
@@ -24,62 +24,46 @@ class CollapseAccordionExample extends React.Component {
         const { collapseOpen1, collapseOpen2, collapseOpen3 } = this.state;
 
         return (
-            <div className="collapse-div">
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen1 ? "true" : "false"}
-                        className={collapseOpen1 ? "" : "collapsed"}
-                        onClick={() => this.toggle(1)}
-                    >
-                        Accordion Group Item #1
-                    </button>
-                </div>
-                <div className={`collapse ${collapseOpen1 ? "show" : ""}`}>
-                    <div className="collapse-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high
-                        life accusamus terry richardson ad squid. 3 wolf moon
-                        officia aute, non cupidatat skateboard dolor brunch.
-                    </div>
-                </div>
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen2 ? "true" : "false"}
-                        className={collapseOpen2 ? "" : "collapsed"}
-                        onClick={() => this.toggle(2)}
-                    >
-                        Accordion Group Item #2
-                    </button>
-                </div>
-                <div className={`collapse ${collapseOpen2 ? "show" : ""}`}>
-                    <div className="collapse-body">
-                        Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                        craft beer farm-to-table, raw denim aesthetic synth
-                        nesciunt you probably haven't heard of them accusamus
-                        labore sustainable VHS.
-                    </div>
-                </div>
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen3 ? "true" : "false"}
-                        className={collapseOpen3 ? "" : "collapsed"}
-                        onClick={() => this.toggle(3)}
-                    >
-                        Accordion Group Item #3
-                    </button>
-                </div>
-                <div className={`collapse ${collapseOpen3 ? "show" : ""}`}>
-                    <div className="collapse-body">
-                        Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                        wolf moon tempor, sunt aliqua put a bird on it squid
-                        single-origin coffee nulla assumenda shoreditch et.
-                        Nihil anim keffiyeh helvetica, craft beer labore wes
-                        anderson cred nesciunt sapiente ea proident.
-                    </div>
-                </div>
-            </div>
+            <Accordion>
+                <AccordionHeader
+                    active={collapseOpen1}
+                    onToggle={() => this.toggle(1)}
+                >
+                    Accordion Group Item #1
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen1}>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch.
+                </AccordionBody>
+
+                <AccordionHeader
+                    active={collapseOpen2}
+                    onToggle={() => this.toggle(2)}
+                >
+                    Accordion Group Item #2
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen2}>
+                    Ad vegan excepteur butcher vice lomo. Leggings occaecat
+                    craft beer farm-to-table, raw denim aesthetic synth nesciunt
+                    you probably haven&apos;t heard of them accusamus labore
+                    sustainable VHS.
+                </AccordionBody>
+
+                <AccordionHeader
+                    active={collapseOpen3}
+                    onToggle={() => this.toggle(3)}
+                >
+                    Accordion Group Item #3
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen3}>
+                    Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                    moon tempor, sunt aliqua put a bird on it squid
+                    single-origin coffee nulla assumenda shoreditch et. Nihil
+                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident.
+                </AccordionBody>
+            </Accordion>
         );
     }
 }
--- stories/Collapse/CollapseGroupsExample.js
+++ stories/Collapse/CollapseGroupsExample.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button, Collapse, Card, CardBody } from "../../src";
+import { Accordion, AccordionHeader, AccordionBody } from "../../src";
 
 class CollapseGroupsExample extends React.Component {
     state = {
         collapseOpen1: true,
         collapseOpen2: true,
-        collapseOpen3: false,
+        collapseOpen3: false
     };
 
     toggle = id => {
@@ -18,74 +18,46 @@ class CollapseGroupsExample extends React.Component {
         const { collapseOpen1, collapseOpen2, collapseOpen3 } = this.state;
 
         return (
-            <div className="collapse-div" role="tablist">
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen1 ? "true" : "false"}
-                        className={collapseOpen1 ? "" : "collapsed"}
-                        onClick={() => this.toggle(1)}
-                    >
-                        Collapsible Group Item #1
-                    </button>
-                </div>
-                <div
-                    className={`collapse ${collapseOpen1 ? "show" : ""}`}
-                    role="tabpanel"
-                    aria-labelledby="heading1"
+            <Accordion>
+                <AccordionHeader
+                    active={collapseOpen1}
+                    onToggle={() => this.toggle(1)}
                 >
-                    <div className="collapse-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high
-                        life accusamus terry richardson ad squid. 3 wolf moon
-                        officia aute, non cupidatat skateboard dolor brunch.
-                    </div>
-                </div>
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen2 ? "true" : "false"}
-                        className={collapseOpen2 ? "" : "collapsed"}
-                        onClick={() => this.toggle(2)}
-                    >
-                        Collapsible Group Item #2
-                    </button>
-                </div>
-                <div
-                    className={`collapse ${collapseOpen2 ? "show" : ""}`}
-                    role="tabpanel"
-                    aria-labelledby="heading2"
+                    Collapsible Group Item #1
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen1}>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch.
+                </AccordionBody>
+
+                <AccordionHeader
+                    active={collapseOpen2}
+                    onToggle={() => this.toggle(2)}
                 >
-                    <div className="collapse-body">
-                        Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                        craft beer farm-to-table, raw denim aesthetic synth
-                        nesciunt you probably haven't heard of them accusamus
-                        labore sustainable VHS.
-                    </div>
-                </div>
-                <div className="collapse-header">
-                    <button
-                        data-toggle="collapse"
-                        aria-expanded={collapseOpen3 ? "true" : "false"}
-                        className={collapseOpen3 ? "" : "collapsed"}
-                        onClick={() => this.toggle(3)}
-                    >
-                        Collapsible Group Item #3
-                    </button>
-                </div>
-                <div
-                    className={`collapse ${collapseOpen3 ? "show" : ""}`}
-                    role="tabpanel"
-                    aria-labelledby="heading3"
+                    Collapsible Group Item #2
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen2}>
+                    Ad vegan excepteur butcher vice lomo. Leggings occaecat
+                    craft beer farm-to-table, raw denim aesthetic synth nesciunt
+                    you probably haven&apos;t heard of them accusamus labore
+                    sustainable VHS.
+                </AccordionBody>
+
+                <AccordionHeader
+                    active={collapseOpen3}
+                    onToggle={() => this.toggle(3)}
                 >
-                    <div className="collapse-body">
-                        Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                        wolf moon tempor, sunt aliqua put a bird on it squid
-                        single-origin coffee nulla assumenda shoreditch et.
-                        Nihil anim keffiyeh helvetica, craft beer labore wes
-                        anderson cred nesciunt sapiente ea proident.
-                    </div>
-                </div>
-            </div>
+                    Collapsible Group Item #3
+                </AccordionHeader>
+                <AccordionBody active={collapseOpen3}>
+                    Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                    moon tempor, sunt aliqua put a bird on it squid
+                    single-origin coffee nulla assumenda shoreditch et. Nihil
+                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident.
+                </AccordionBody>
+            </Accordion>
         );
     }
 }
