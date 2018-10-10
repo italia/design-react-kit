79127fac82e5a4fd111d822e8f78ea7e91221588
Nicola Ferracin <nicola.ferracin@gmail.com>
2018-10-10 23:16:09 +0700

    Popover: fix display of popover with different placements

--- stories/Popover/PopoverPositionExample.js
+++ stories/Popover/PopoverPositionExample.js
@@ -1,66 +1,73 @@
 import React from "react";
-import { Button, Popover, PopoverHeader, PopoverBody } from "../../src";
+import { Button, Popover, PopoverBody } from "../../src";
 
 class PopoverItem extends React.Component {
-  state = {
-    popoverOpen: false
-  };
+    state = {
+        popoverOpen: false
+    };
 
-  toggle = () => {
-    this.setState({
-      popoverOpen: !this.state.popoverOpen
-    });
-  };
+    toggle = () => {
+        this.setState({
+            popoverOpen: !this.state.popoverOpen
+        });
+    };
 
-  render() {
-    const id = `Popover-${this.props.id}`;
-    // Avoid Jest complaints
-    const target = () => document.getElementById(id);
+    render() {
+        const id = `Popover-${this.props.id}`;
+        // Avoid Jest complaints
+        const target = () => document.getElementById(id);
 
-    return (
-      <span>
-        <Button
-          className="mr-1"
-          color="secondary"
-          id={id}
-          onClick={this.toggle}
-        >
-          Popover {this.props.item.text}
-        </Button>
-        <Popover
-          placement={this.props.item.placement}
-          isOpen={this.state.popoverOpen}
-          target={target}
-          toggle={this.toggle}
-        >
-          <PopoverBody>
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus
-          </PopoverBody>
-        </Popover>
-      </span>
-    );
-  }
+        return (
+            <span>
+                <Button
+                    className="mr-1"
+                    color="secondary"
+                    id={id}
+                    onClick={this.toggle}
+                >
+                    Popover {this.props.item.text}
+                </Button>
+                <Popover
+                    placement={this.props.item.placement}
+                    isOpen={this.state.popoverOpen}
+                    target={target}
+                    toggle={this.toggle}
+                >
+                    <PopoverBody>
+                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus
+                    </PopoverBody>
+                </Popover>
+            </span>
+        );
+    }
 }
 
 class PopoverPositionExample extends React.Component {
-  state = {
-    popovers: [
-      { placement: "top", text: "in alto" },
-      { placement: "bottom", text: "in basso" },
-      { placement: "left", text: "a sinistra" },
-      { placement: "right", text: "a destra" }
-    ]
-  };
+    state = {
+        popovers: [
+            { placement: "top", text: "in alto" },
+            { placement: "bottom", text: "in basso" },
+            { placement: "left", text: "a sinistra" },
+            { placement: "right", text: "a destra" }
+        ]
+    };
 
-  render() {
-    return (
-      <div style={{ padding: 200 }}>
-        {this.state.popovers.map((popover, i) => {
-          return <PopoverItem key={i} item={popover} id={i} />;
-        })}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}
+            >
+                {this.state.popovers.map((popover, i) => (
+                    <PopoverItem key={i} item={popover} id={i} />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default PopoverPositionExample;
