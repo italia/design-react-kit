52d4706b2ac7492c3b9634707c613438a3a9cfff
Sanyam Dogra <sdlord07@gmail.com>
2019-06-13 14:44:15 +0530

    Added padding to StoryPreview

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,22 @@
+import React from "react";
 import { addParameters, addDecorator, configure } from '@storybook/react';
 import { default as theme } from './theme.js';
 
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import "../assets/docs.min.css";
 import "../assets/css/fonts.css";
+
+const styles = {
+                    position: "relative",
+                    boxSizing: "border-box",
+                    margin: "0 0 0 0",
+                    padding: "50px 35px",
+                    border: "1.5px dashed #e5e5e5",
+                    backgroundColor: "#ffffff",
+                    transition: "background-color 0.2s",
+                    textAlign: "initial"
+  };
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
 
 addParameters({
     options: {
@@ -16,6 +29,7 @@ addParameters({
     },
 });
 
+addDecorator(CenterDecorator);
 function loadStories() {
     require('../stories/index.js');
     // You can require as many stories as you need.
