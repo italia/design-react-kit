e5460ecfffec8f3b4a55593a8425c73179cdb27d
Francesco Zaia <francesco.zaia@gmail.com>
2019-06-16 09:14:56 +0200

    Update to StoryBook 5.1.8

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { addParameters, addDecorator, configure } from '@storybook/react';
-import { default as theme } from './theme.js';
+import React from 'react';
+import {addParameters, addDecorator, configure} from '@storybook/react';
+import {default as theme} from './theme.js';
 
-import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
-import "../assets/docs.min.css";
-import "../assets/css/fonts.css";
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import '../assets/docs.min.css';
+import '../assets/css/fonts.css';
 
 const styles = {
-                    position: "relative",
-                    boxSizing: "border-box",
-                    margin: "0 0 0 0",
-                    padding: "50px 35px",
-                    border: "1.5px dashed #e5e5e5",
-                    backgroundColor: "#ffffff",
-                    transition: "background-color 0.2s",
-                    textAlign: "initial"
-  };
+    position: 'relative',
+    boxSizing: 'border-box',
+    margin: '0',
+    padding: '48px 32px',
+    border: '1px solid #F3F3F5',
+    backgroundColor: '#FFF',
+    transition: 'background-color 0.2s',
+    textAlign: 'initial'
+};
 const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
 
 addParameters({
@@ -30,6 +30,7 @@ addParameters({
 });
 
 addDecorator(CenterDecorator);
+
 function loadStories() {
     require('../stories/index.js');
     // You can require as many stories as you need.
