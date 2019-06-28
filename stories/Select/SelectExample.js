471195868c93836a6ee0d66287a7b16883f54ac2
Sanyam Dogra <sdlord07@gmail.com>
2019-06-28 15:44:34 +0530

    Added double quotes for consistency

--- stories/Select/SelectExample.js
+++ stories/Select/SelectExample.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Select from 'react-select';
+import React from "react";
+import Select from "react-select";
 import PropTypes from "prop-types";
 
 const defaultOptions = [
