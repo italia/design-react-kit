b5f84d39e7c2cf7253ac7c3b0e818e8e72c3edac
Sanyam Dogra <sdlord07@gmail.com>
2019-06-28 17:55:57 +0530

    Added label for select component

--- stories/Select/SelectExample.js
+++ stories/Select/SelectExample.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Select from "react-select";
 import PropTypes from "prop-types";
+import { Label } from "../../src";
 
 const defaultOptions = [
-  { value: 'Value 1', label: 'Opzione 1' },
-  { value: 'Value 2', label: 'Opzione 2' },
-  { value: 'Value 3', label: 'Opzione 3' },
-  { value: 'Value 4', label: 'Opzione 4' },
-  { value: 'Value 5', label: 'Opzione 5' },
+    { value: "Value 1", label: "Opzione 1" },
+    { value: "Value 2", label: "Opzione 2" },
+    { value: "Value 3", label: "Opzione 3" },
+    { value: "Value 4", label: "Opzione 4" },
+    { value: "Value 5", label: "Opzione 5" }
 ];
 
 const multiOptions = [
-  { value: 'Value 1', label: 'Lorem ipsum dolor sit amet' },
-  { value: 'Value 2', label: 'Duis vestibulum eleifend libero' },
-  { value: 'Value 3', label: 'Phasellus pretium orci sed odio tempus' },
-  { value: 'Value 4', label: 'Vestibulum bibendum ex vel augue porttitor sodales' },
-  { value: 'Value 5', label: 'Praesent quis elementum turpis' },
+    { value: "Value 1", label: "Lorem ipsum dolor sit amet" },
+    { value: "Value 2", label: "Duis vestibulum eleifend libero" },
+    { value: "Value 3", label: "Phasellus pretium orci sed odio tempus" },
+    {
+        value: "Value 4",
+        label: "Vestibulum bibendum ex vel augue porttitor sodales"
+    },
+    { value: "Value 5", label: "Praesent quis elementum turpis" }
 ];
 
 export const groupOneOptions = [
@@ -40,41 +44,51 @@ export const groupedOptions = [
 ];
 
 class SelectExample extends React.Component {
-  state = {
-    selectedOption: null,
-    placeholder: "Scegli una opzione",
-    isDisabled: false,
-    isSearchable: false,
-    isMulti: false,
-    isClearable: false,
-  };
-  handleChange = selectedOption => {
-    this.setState({ selectedOption });
-  };
-  render() {
-    const { selectedOption, disabled, search, multi, group, reset } = this.props;
-    let options = defaultOptions;
+    state = {
+        selectedOption: null,
+        placeholder: "Scegli una opzione",
+        isDisabled: false,
+        isSearchable: false,
+        isMulti: false,
+        isClearable: false
+    };
+    handleChange = selectedOption => {
+        this.setState({ selectedOption });
+    };
+    render() {
+        const {
+            selectedOption,
+            disabled,
+            search,
+            multi,
+            group,
+            reset
+        } = this.props;
+        let options = defaultOptions;
         if (search) {
             options = multiOptions;
         } else if (multi) {
-          options = defaultOptions;
-        }else if (group) {
-          options = groupedOptions;
-        } 
+            options = defaultOptions;
+        } else if (group) {
+            options = groupedOptions;
+        }
 
-    return (
-      <Select
-        value={selectedOption}
-        onChange={this.handleChange}
-        options={options}
-        placeholder={this.state.placeholder}
-        isDisabled={disabled ? true : false}
-        isSearchable={search ? true : false}
-        isMulti={multi ? true : false}
-        isClearable={reset ? true : false}
-      />
-    );
-  }
+        return (
+            <div class="bootstrap-select-wrapper">
+                <Label for="selectExample">Etichetta di esempio</Label>
+                <Select
+                    value={selectedOption}
+                    onChange={this.handleChange}
+                    options={options}
+                    placeholder={this.state.placeholder}
+                    isDisabled={disabled ? true : false}
+                    isSearchable={search ? true : false}
+                    isMulti={multi ? true : false}
+                    isClearable={reset ? true : false}
+                />
+            </div>
+        );
+    }
 }
 
 export default SelectExample;
@@ -85,5 +99,5 @@ SelectExample.propTypes = {
     search: PropTypes.bool,
     multi: PropTypes.bool,
     group: PropTypes.bool,
-    placeholder: PropTypes.string,
+    placeholder: PropTypes.string
 };
