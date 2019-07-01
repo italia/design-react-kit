f75dd22ef103664a23b1f2273c4fe1a510b87d91
Sanyam Dogra <sdlord07@gmail.com>
2019-07-01 14:31:38 +0530

    Added few changes

--- stories/Autocomplete/AutocompleteExample.js
+++ stories/Autocomplete/AutocompleteExample.js
@@ -248,62 +248,97 @@
 //     placeholder: PropTypes.string
 // };
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { components } from "react-select";
+import AsyncSelect from "react-select/async";
 
-import AsyncSelect from 'react-select/async';
+const DropdownIndicator = props => {
+    return (
+        <components.DropdownIndicator {...props}>
+            <span style={{ padding: "0px 5px" }} aria-hidden="true">
+                <svg class="icon icon-sm">
+                    <use xlinkHref="/svg/sprite.svg#it-search" />
+                </svg>
+            </span>
+        </components.DropdownIndicator>
+    );
+};
+
+const Input = props => {
+    if (props.isHidden) {
+        return <components.Input {...props} />;
+    }
+    return (
+        <div style={{ border: `1px dotted blue}` }}>
+            <components.Input {...props} />
+        </div>
+    );
+};
 
 const multiOptions = [
-        { value: "1", label: "Abruzzo" },
-        { value: "2", label: "Basilicata" },
-        { value: "3", label: "Calabria" },
-        { value: "4", label: "Campania" },
-        { value: "5", label: "Emilia Romagna" },
-        { value: "6", label: "Friuli Venezia Giulia" },
-        { value: "7", label: "Lazio" },
-        { value: "8", label: "Liguria" },
-        { value: "9", label: "Lombardia" },
-        { value: "10", label: "Marche" },
-        { value: "11", label: "Molise" },
-        { value: "12", label: "Piemonte" },
-        { value: "13", label: "Puglia" },
-        { value: "14", label: "Sardegna" },
-        { value: "15", label: "Sicilia" },
-        { value: "16", label: "Toscana" },
-        { value: "17", label: "Trentino Alto Adige" },
-        { value: "18", label: "Umbria" },
-        { value: "19", label: "Valle d'Aosta" },
-        { value: "20", label: "Veneto" }
-    ];
+    { value: "1", label: "Abruzzo" },
+    { value: "2", label: "Basilicata" },
+    { value: "3", label: "Calabria" },
+    { value: "4", label: "Campania" },
+    { value: "5", label: "Emilia Romagna" },
+    { value: "6", label: "Friuli Venezia Giulia" },
+    { value: "7", label: "Lazio" },
+    { value: "8", label: "Liguria" },
+    { value: "9", label: "Lombardia" },
+    { value: "10", label: "Marche" },
+    { value: "11", label: "Molise" },
+    { value: "12", label: "Piemonte" },
+    { value: "13", label: "Puglia" },
+    { value: "14", label: "Sardegna" },
+    { value: "15", label: "Sicilia" },
+    { value: "16", label: "Toscana" },
+    { value: "17", label: "Trentino Alto Adige" },
+    { value: "18", label: "Umbria" },
+    { value: "19", label: "Valle d'Aosta" },
+    { value: "20", label: "Veneto" }
+];
 
-const filterOptions = (inputValue) => {
-  return multiOptions.filter(i =>
-    i.label.toLowerCase().includes(inputValue.toLowerCase())
-  );
+const filterOptions = inputValue => {
+    return multiOptions.filter(i =>
+        i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
 };
 
 const loadOptions = (inputValue, callback) => {
-  setTimeout(() => {
-    callback(filterOptions(inputValue));
-  }, 1000);
+    setTimeout(() => {
+        callback(filterOptions(inputValue));
+    }, 1000);
 };
 
 export default class AutocompleteExample extends React.Component {
-  state = { inputValue: '' };
-  handleInputChange = (newValue) => {
-    const inputValue = newValue.replace(/\W/g, '');
-    this.setState({ inputValue });
-    return inputValue;
-  };
-  render() {
-    return (
-      <div className="form-group">
-        <AsyncSelect
-          cacheOptions
-          loadOptions={loadOptions}
-          defaultOptions
-          onInputChange={this.handleInputChange}
-        />
-      </div>
-    );
-  }
+    state = { inputValue: "", placeholder: "Testo da cercare" };
+
+    handleInputChange = newValue => {
+        const inputValue = newValue.replace(/\W/g, "");
+        this.setState({ inputValue });
+        return inputValue;
+    };
+    render() {
+        return (
+            <div className="form-group">
+                <AsyncSelect
+                    id="autocomplete-regioni"
+                    components={{
+                        DropdownIndicator,
+                        Input,
+                        IndicatorSeparator: null
+                    }}
+                    cacheOptions
+                    loadOptions={loadOptions}
+                    defaultOptions
+                    placeholder={this.state.placeholder}
+                    onInputChange={this.handleInputChange}
+                    
+                />
+                <label for="autocomplete-regioni" class="sr-only">
+                    Cerca nel sito
+                </label>
+            </div>
+        );
+    }
 }
