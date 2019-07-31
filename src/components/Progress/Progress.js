742ec477084ccc218293dfd1f772ecbe7e7f300a
Sanyam Dogra <sdlord07@gmail.com>
2019-07-31 22:36:10 +0530

    Ran standard fix

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,88 +1,88 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 const propTypes = {
-    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-    className: PropTypes.string,
-    value: PropTypes.number,
-    label: PropTypes.string,
-    intermediate: PropTypes.bool,
-    color: PropTypes.string
-};
-
-const defaultProps = {
-    tag: "div",
-    role: "progressbar",
-    intermediate: false
-};
-
-class Progress extends React.Component {
-    render() {
-        const {
-            className,
-            tag: Tag,
-            value,
-            label,
-            intermediate,
-            color,
-            ...attributes
-        } = this.props;
-        const wrapperClasses = classNames("progress-bar-wrapper");
-        const secondaryWrapperClasses = classNames(
-            className,
-            intermediate ? "progress-indeterminate" : false,
-            color ? "progress-color" : false,
-            "progress"
-        );
-        const classes = classNames(
-            className,
-            "progress-bar",
-            color ? `bg-${this.props.color}` : false
-        );
-
-        if (label && value) {
-            return (
-                <Tag className={wrapperClasses}>
-                    <div className="progress-bar-label">
-                        <span className="sr-only">{this.props.label}</span>
-                        {this.props.value + "%"}
-                    </div>
-                    <Tag className={secondaryWrapperClasses}>
-                        <div
-                            {...attributes}
-                            className={classes}
-                            role="progressbar"
-                            style={{ width: this.props.value + "%" }}
-                            aria-valuenow={this.props.value}
-                            aria-valuemin="0"
-                            aria-valuemax="100"
-                        />
-                    </Tag>
-                </Tag>
-            );
-        }
-
-        return (
-            <Tag className={secondaryWrapperClasses}>
-                <div className="progress-bar-label">
-                    <span className="sr-only">{this.props.label}</span>
-                </div>
-                <div
-                    {...attributes}
-                    className={classes}
-                    role="progressbar"
-                    style={{ width: this.props.value + "%" }}
-                    aria-valuenow={this.props.value}
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                />
-            </Tag>
-        );
-    }
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
+  value: PropTypes.number,
+  label: PropTypes.string,
+  intermediate: PropTypes.bool,
+  color: PropTypes.string
 }
 
-Progress.propTypes = propTypes;
-Progress.defaultProps = defaultProps;
+const defaultProps = {
+  tag: 'div',
+  role: 'progressbar',
+  intermediate: false
+}
 
-export default Progress;
+class Progress extends React.Component {
+  render () {
+    const {
+      className,
+      tag: Tag,
+      value,
+      label,
+      intermediate,
+      color,
+      ...attributes
+    } = this.props
+    const wrapperClasses = classNames('progress-bar-wrapper')
+    const secondaryWrapperClasses = classNames(
+      className,
+      intermediate ? 'progress-indeterminate' : false,
+      color ? 'progress-color' : false,
+      'progress'
+    )
+    const classes = classNames(
+      className,
+      'progress-bar',
+      color ? `bg-${this.props.color}` : false
+    )
+
+    if (label && value) {
+      return (
+        <Tag className={wrapperClasses}>
+          <div className='progress-bar-label'>
+            <span className='sr-only'>{this.props.label}</span>
+            {this.props.value + '%'}
+          </div>
+          <Tag className={secondaryWrapperClasses}>
+            <div
+              {...attributes}
+              className={classes}
+              role='progressbar'
+              style={{ width: this.props.value + '%' }}
+              aria-valuenow={this.props.value}
+              aria-valuemin='0'
+              aria-valuemax='100'
+            />
+          </Tag>
+        </Tag>
+      )
+    }
+
+    return (
+      <Tag className={secondaryWrapperClasses}>
+        <div className='progress-bar-label'>
+          <span className='sr-only'>{this.props.label}</span>
+        </div>
+        <div
+          {...attributes}
+          className={classes}
+          role='progressbar'
+          style={{ width: this.props.value + '%' }}
+          aria-valuenow={this.props.value}
+          aria-valuemin='0'
+          aria-valuemax='100'
+        />
+      </Tag>
+    )
+  }
+}
+
+Progress.propTypes = propTypes
+Progress.defaultProps = defaultProps
+
+export default Progress
