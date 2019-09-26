7b57d185c3b2629492bd881e555d7c3c9ce155bc
dej611 <dej611@gmail.com>
2019-09-26 09:36:37 +0200

    linting fixes

--- stories/Templates/Templates.stories.js
+++ stories/Templates/Templates.stories.js
@@ -1,6 +1,5 @@
-import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, text, select } from '@storybook/addon-knobs/react'
+import { withKnobs} from '@storybook/addon-knobs/react'
 import { withA11y } from '@storybook/addon-a11y'
 import { withInfo } from '@storybook/addon-info'
 
