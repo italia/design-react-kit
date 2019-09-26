5308fc814f2a358083057985a6a3d702a0c553ac
dej611 <dej611@gmail.com>
2019-09-26 09:47:21 +0200

    Prettier round 1

--- stories/Templates/Templates.stories.js
+++ stories/Templates/Templates.stories.js
@@ -1,13 +1,16 @@
 import { storiesOf } from '@storybook/react'
-import { withKnobs} from '@storybook/addon-knobs/react'
+import { withKnobs } from '@storybook/addon-knobs/react'
 import { withA11y } from '@storybook/addon-a11y'
 import { withInfo } from '@storybook/addon-info'
 
-import Homepage from './homepage/Homepage';
+import Homepage from './homepage/Homepage'
 
 storiesOf('Introduzione/Introduzione', module)
-.addDecorator(withA11y)
-.addDecorator(withKnobs)
-  .add('Homepage Template', withInfo({
-    // text: Esempi
-  })(Homepage))
+  .addDecorator(withA11y)
+  .addDecorator(withKnobs)
+  .add(
+    'Homepage Template',
+    withInfo({
+      // text: Esempi
+    })(Homepage)
+  )
