eb1520f4ce3a50e658f225eb1577bfa056197d08
Francesco Zaia <francesco.zaia@gmail.com>
2019-10-02 15:33:02 +0200

    Update Collapse.js

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -83,7 +83,7 @@ class Collapse extends Component {
 
   onExiting(node) {
     // getting this variable triggers a reflow
-    const _unused = node.offsetHeight;
+    // const _unused = node.offsetHeight;
     this.setState({ height: 0 })
     this.props.onExiting(node)
   }
