6206f4f9317d3eee913ddbae74955042c253f832
ankingcodes <ankush.bhardwaj0@gmail.com>
2019-10-08 00:19:24 +0530

    modified accept button

--- stories/Cookiebar/Cookiebar.stories.js
+++ stories/Cookiebar/Cookiebar.stories.js
@@ -9,16 +9,34 @@ import Esempi from './docs/example.md'
 import { CookieBar,CookieBarButtons,CookieBarButton } from '../../src'
 
 class CookieBarComponent extends React.Component {
+    defaultState = {
+        clicked : true
+    }
+    state = {
+        ...this.defaultState,
+        clicked: false
+    }
+    toggle = () => {
+        this.setState({
+            ...this.defaultState,
+            clicked: !this.state.clicked
+        })
+    }
+
     render(){
+        if(this.state.clicked==false){
       return(
+    <div style = {{display: "block"}}>
       <CookieBar> 
             <p>Questo sito utilizza cookie tecnici, analytics e di terze parti. <br/>Proseguendo nella navigazione accetti l’utilizzo dei cookie.</p> 
             <CookieBarButtons>  
              <CookieBarButton href="#">Preferenze<span class="sr-only">cookies</span></CookieBarButton>
-             <CookieBarButton>Accetto<span class="sr-only"> i cookies</span></CookieBarButton> 
+             <CookieBarButton onClick={()=>this.toggle()}>Accetto<span class="sr-only"> i cookies</span></CookieBarButton> 
             </CookieBarButtons>
          </CookieBar>
+    </div>
       )}
+    }
 }
 
 storiesOf('Componenti/Cookiebar',module)
