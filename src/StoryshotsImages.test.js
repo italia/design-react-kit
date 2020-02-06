cd61d60e46cab95c377c3cd82a54d82d99829ea2
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2020-02-06 19:00:58 +0100

    Remove duplicate storyshots tests

--- src/StoryshotsImages.test.js
+++
@@ -1,7 +0,0 @@
-// File is located here in order to avoid a custom jest.config.js
-// Current config is inherited from react-scripts
-import initStoryshots from '@storybook/addon-storyshots'
-
-initStoryshots({
-  suite: 'Image Storyshots'
-})
