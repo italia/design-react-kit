5912bbf20f6ffdda2e0fbcb983fd3f9a73e82c66
dej611 <dej611@gmail.com>
2020-10-03 00:04:49 +0200

    :bug: Fix gatsby build with SSR mocks

--- src/components/Forward/Forward.js
+++ src/components/Forward/Forward.js
@@ -2,6 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
+// SSR does have no access to the Element global variable, so mock it
+if (typeof window === 'undefined') {
+  global.Element = typeof Element === 'undefined' ? function() {} : Element
+}
+
 const propTypes = {
   className: PropTypes.string,
   scrollToRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) })
