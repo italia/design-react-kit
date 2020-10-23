31f107c8743c7852b603ce295b2713855c6564a7
Sanyam Dogra <sdlord07@gmail.com>
2020-10-23 23:11:26 +0530

    Expanded nested example

--- stories/Collapse/CollapseNestedExample.js
+++ stories/Collapse/CollapseNestedExample.js
@@ -5,6 +5,9 @@ const CollapseNestedExample = () => {
   const [collapseOpen1, setCollapseOpen1] = useState(true)
   const [collapseOpen2, setCollapseOpen2] = useState(false)
   const [collapseOpen3, setCollapseOpen3] = useState(false)
+  const [nestedCollapseOpen1, setNestedCollapseOpen1] = useState(true)
+  const [nestedCollapseOpen2, setNestedCollapseOpen2] = useState(false)
+  const [nestedCollapseOpen3, setNestedCollapseOpen3] = useState(false)
   return (
     <Accordion>
       <AccordionHeader
@@ -13,7 +16,41 @@ const CollapseNestedExample = () => {
         Collapse header #1
       </AccordionHeader>
       <AccordionBody active={collapseOpen1}>
-        <CollapseNestedPanel />
+        <Accordion>
+          <AccordionHeader
+            active={nestedCollapseOpen1}
+            onToggle={() => setNestedCollapseOpen1(!nestedCollapseOpen1)}>
+            Nested panel header #1
+          </AccordionHeader>
+          <AccordionBody active={nestedCollapseOpen1}>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+            skateboard dolor brunch.
+          </AccordionBody>
+
+          <AccordionHeader
+            active={nestedCollapseOpen2}
+            onToggle={() => setNestedCollapseOpen2(!nestedCollapseOpen2)}>
+            Nested panel header #2
+          </AccordionHeader>
+          <AccordionBody active={nestedCollapseOpen2}>
+            Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+            farm-to-table, raw denim aesthetic synth nesciunt you probably
+            haven&apos;t heard of them accusamus labore sustainable VHS.
+          </AccordionBody>
+
+          <AccordionHeader
+            active={nestedCollapseOpen3}
+            onToggle={() => setNestedCollapseOpen3(!nestedCollapseOpen3)}>
+            Nested panel header #3
+          </AccordionHeader>
+          <AccordionBody active={nestedCollapseOpen3}>
+            Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
+            tempor, sunt aliqua put a bird on it squid single-origin coffee
+            nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
+            beer labore wes anderson cred nesciunt sapiente ea proident.
+          </AccordionBody>
+        </Accordion>
       </AccordionBody>
 
       <AccordionHeader
@@ -31,49 +68,6 @@ const CollapseNestedExample = () => {
         active={collapseOpen3}
         onToggle={() => setCollapseOpen3(!collapseOpen3)}>
         Collapse header #3
-      </AccordionHeader>
-      <AccordionBody active={collapseOpen3}>
-        Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
-        anderson cred nesciunt sapiente ea proident.
-      </AccordionBody>
-    </Accordion>
-  )
-}
-const CollapseNestedPanel = () => {
-  const [collapseOpen1, setCollapseOpen1] = useState(false)
-  const [collapseOpen2, setCollapseOpen2] = useState(false)
-  const [collapseOpen3, setCollapseOpen3] = useState(false)
-
-  return (
-    <Accordion>
-      <AccordionHeader
-        active={collapseOpen1}
-        onToggle={() => setCollapseOpen1(!collapseOpen1)}>
-        Nested panel header #1
-      </AccordionHeader>
-      <AccordionBody active={collapseOpen1}>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-        terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-        skateboard dolor brunch.
-      </AccordionBody>
-
-      <AccordionHeader
-        active={collapseOpen2}
-        onToggle={() => setCollapseOpen2(!collapseOpen2)}>
-        Nested panel header #2
-      </AccordionHeader>
-      <AccordionBody active={collapseOpen2}>
-        Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-        farm-to-table, raw denim aesthetic synth nesciunt you probably
-        haven&apos;t heard of them accusamus labore sustainable VHS.
-      </AccordionBody>
-
-      <AccordionHeader
-        active={collapseOpen3}
-        onToggle={() => setCollapseOpen3(!collapseOpen3)}>
-        Nested panel header #3
       </AccordionHeader>
       <AccordionBody active={collapseOpen3}>
         Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
