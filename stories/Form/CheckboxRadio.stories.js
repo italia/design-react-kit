72f549d74e9a4b24a72e639eede68e3dc3bf5848
dej611 <dej611@gmail.com>
2020-11-28 10:57:22 +0100

    :sparkles: Complete Form doc with current components

--- stories/Form/CheckboxRadio.stories.js
+++ stories/Form/CheckboxRadio.stories.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-import { Form, FormGroup, Label, Input, FormText } from '../../src'
-
-import CheckboxGroupsExample from './CheckboxRadio/CheckboxGroupsExample'
-import RadioGroupsExample from './CheckboxRadio/RadioGroupsExample'
+import { Row, Col, Form, FormGroup, Label, Input, FormText } from '../../src'
 
 import CheckboxRadio from './docs/CheckboxRadio/CheckboxRadio.md'
 import Disabilitato from './docs/CheckboxRadio/Disabilitato.md'
@@ -138,7 +135,135 @@ export const __Disabilitato = () => (
 
 export const _Gruppi = () => (
   <section>
-    <CheckboxGroupsExample />
-    <RadioGroupsExample />
+    <Row className="m-5">
+      <Col md={5}>
+        <fieldset>
+          <legend>Checkbox</legend>
+          <Form>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox1" type="checkbox" defaultChecked />
+              <Label for="checkbox1" check>
+                Checkbox selezionato
+              </Label>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox2" type="checkbox" />
+              <Label for="checkbox2" check>
+                Checkbox non selezionato
+              </Label>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox3" type="checkbox" disabled />
+              <Label for="checkbox3" check>
+                Checkbox disabilitato non selezionato
+              </Label>
+            </FormGroup>
+          </Form>
+        </fieldset>
+      </Col>
+      <Col md={2} />
+      <Col md={5}>
+        <fieldset>
+          <legend>Checkbox</legend>
+          <Form>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox4" type="checkbox" defaultChecked />
+              <Label for="checkbox4" check>
+                Checkbox selezionato
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox5" type="checkbox" />
+              <Label for="checkbox5" check>
+                Checkbox non selezionato
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="checkbox6" type="checkbox" disabled />
+              <Label for="checkbox6" check>
+                Checkbox disabilitato non selezionato
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+          </Form>
+        </fieldset>
+      </Col>
+    </Row>
+    <Row className="m-5">
+      <Col md={5}>
+        <fieldset>
+          <legend>Radio</legend>
+          <Form>
+            <FormGroup check className="form-check-group">
+              <Input id="radio1" name="group1" type="radio" defaultChecked />
+              <Label for="radio1" check>
+                Opzione 1
+              </Label>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="radio2" name="group1" type="radio" />
+              <Label for="radio2" check>
+                Opzione 2
+              </Label>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="radio3" name="group1" type="radio" disabled />
+              <Label for="radio3" check>
+                Opzione 3
+              </Label>
+            </FormGroup>
+          </Form>
+        </fieldset>
+      </Col>
+      <Col md={2} />
+      <Col md={5}>
+        <fieldset>
+          <legend>Radio</legend>
+          <Form>
+            <FormGroup check className="form-check-group">
+              <Input id="radio4" name="group2" type="radio" defaultChecked />
+              <Label for="radio4" check>
+                Opzione 1
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="radio5" name="group2" type="radio" />
+              <Label for="radio5" check>
+                Opzione 2
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+            <FormGroup check className="form-check-group">
+              <Input id="radio6" name="group2" type="radio" disabled />
+              <Label for="radio6" check>
+                Opzione 3
+              </Label>
+              <FormText color="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas molestie libero
+              </FormText>
+            </FormGroup>
+          </Form>
+        </fieldset>
+      </Col>
+    </Row>
   </section>
 )
