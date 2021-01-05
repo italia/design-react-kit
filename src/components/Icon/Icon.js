e3f0f9923adcc95b8f6a62083efad044c0afc4dd
dej611 <dej611@gmail.com>
2021-01-05 20:38:50 +0100

    :bug: Use svgr to create SVG icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import iconSprite from 'bootstrap-italia/dist/svg/sprite.svg'
+import * as Svgs from './assets'
+
+const kebabCase = string =>
+  string
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/\s+/g, '-')
+    .toLowerCase()
+
+const icons = Object.keys(Svgs).reduce((memo, pascalName) => {
+  memo[kebabCase(pascalName)] = Svgs[pascalName]
+  return memo
+}, {})
 
 const propTypes = {
   className: PropTypes.string,
@@ -18,6 +29,8 @@ const defaultProps = {
   padding: false
 }
 
+const EmptyIcon = props => <svg {...props}></svg>
+
 const Icon = ({ color, size, icon, className, padding, ...attributes }) => {
   const classes = classNames(
     'icon',
@@ -29,11 +42,9 @@ const Icon = ({ color, size, icon, className, padding, ...attributes }) => {
     },
     size
   )
-  return (
-    <svg className={classes} {...attributes}>
-      <use href={`${iconSprite}#${icon}`} />
-    </svg>
-  )
+
+  const IconComponent = icons[icon] || EmptyIcon
+  return <IconComponent className={classes} {...attributes} />
 }
 
 Icon.propTypes = propTypes
