6e3c00fe65e588c5c6efb985a4e8f43eae32525d
dej611 <dej611@gmail.com>
2021-01-28 09:18:49 +0100

    :arrow_up: Bump to BI 1.4.3 and new Icons with fix

--- src/components/Icon/assets/ItPiattaforme.js
+++ src/components/Icon/assets/ItPiattaforme.js
@@ -3,8 +3,9 @@ import * as React from 'react'
 function SvgItPiattaforme(props) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-      <path d="M9 1.14L15.94 5 9 8.86 2.06 5 9 1.14M9 0L0 5l9 5 9-5-9-5zM18 11.72l-9 5.01-9-5.01 1-.54 8 4.44 7.97-4.47 1.03.57z" />
-      <path d="M18 8.51l-9 5-9-5 1-.55 8 4.44 7.97-4.46 1.03.57z" />
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d="M12 4.14L18.94 8 12 11.86 5.06 8 12 4.14M12 3L3 8l9 5 9-5-9-5zM21 14.72l-9 5.01-9-5.01 1-.54 8 4.44 7.97-4.47 1.03.57z" />
+      <path d="M21 11.51l-9 5-9-5 1-.55 8 4.44 7.97-4.46 1.03.57z" />
     </svg>
   )
 }
