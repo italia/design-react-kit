144088283d618fb9318d67ae39cce9afbfc7fe6b
dej611 <dej611@gmail.com>
2021-02-13 16:29:32 +0100

    :sparkles: Add Card stories doc

--- stories/Card/Card.stories.js
+++ stories/Card/Card.stories.js
@@ -1,33 +1,28 @@
 import React from 'react'
 
 import {
+  Button,
   Card,
   CardBody,
   CardTitle,
   CardText,
-  Button,
   CardCategory,
   CardSignature,
   CardReadMore,
   Icon,
   Row,
   Col,
+  CardTag,
   CardTagsHeader
 } from '../../src'
 
-import SimpleCard from './docs/SimpleCard.md'
-import SimpleCardMultipleColumns from './docs/SimpleCardMultipleColumns.md'
-import SimpleArticle from './docs/SimpleArticle.md'
-import CardIcon from './docs/CardIcon.md'
-import CardShading from './docs/CardShading.md'
-import BigCard from './docs/BigCard.md'
-import BigCardTag from './docs/BigCardTag.md'
-import CardImage from './docs/CardImage.md'
-import SpecialCard from './docs/SpecialCard.md'
+export default {
+  title: 'Componenti/Cards'
+}
 
-const SimpleCardComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-4">
+export const SimpleCard = () => (
+  <Row>
+    <Col xs="12" lg="4">
       {/* start card */}
       <Card>
         <CardBody>
@@ -42,14 +37,18 @@ const SimpleCardComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
 
-const SimpleCardMultipleColumnsComponent = () => (
-  <div className="row">
+SimpleCard.story = {
+  name: 'Card semplice'
+}
+
+export const SimpleCardMultipleColumns = () => (
+  <Row>
     {[1, 2, 3].map(i => (
-      <div className="col-12 col-lg-4" key={i}>
+      <Col xs="12" lg="4" key={i}>
         <Card>
           <CardBody>
             <CardTitle tag="h5">
@@ -62,14 +61,18 @@ const SimpleCardMultipleColumnsComponent = () => (
             </CardText>
           </CardBody>
         </Card>
-      </div>
+      </Col>
     ))}
-  </div>
+  </Row>
 )
 
-const SimpleArticleComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+SimpleCardMultipleColumns.story = {
+  name: 'Card semplice su più colonne'
+}
+
+export const SimpleArticle = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* start card */}
       <Card>
         <CardBody>
@@ -86,13 +89,17 @@ const SimpleArticleComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
 
-const SimpleArticleDateIconComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+SimpleArticle.story = {
+  name: 'Articolo semplice'
+}
+
+export const SimpleArticleWithIcon = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* start card */}
       <Card>
         <CardBody>
@@ -111,13 +118,16 @@ const SimpleArticleDateIconComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
+SimpleArticleWithIcon.story = {
+  name: 'Articolo con Icona'
+}
 
-const CardIconComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+export const CardIcon = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
       <Card>
         <CardBody>
@@ -142,13 +152,49 @@ const CardIconComponent = () => (
       </Card>
 
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
+CardIcon.story = {
+  name: 'Card con Icona'
+}
 
-const CardShadingComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+export const CardIconAdvanced = () => (
+  <Row>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card>
+        <CardBody>
+          <CardCategory iconName="it-file">
+            <span className="text">(2) Files</span>
+          </CardCategory>
+          <a href="#">
+            <CardTitle tag="h5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor…
+            </CardTitle>
+          </a>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </CardText>
+          <a className="simple-link" href="#">
+            Link
+          </a>
+        </CardBody>
+      </Card>
+
+      {/* end card */}
+    </Col>
+  </Row>
+)
+CardIconAdvanced.story = {
+  name: 'Card con Icona alternativa'
+}
+
+export const CardWithShading = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
       <Card space className="card-bg">
         <CardBody>
@@ -164,13 +210,17 @@ const CardShadingComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
 
-const BigCardComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-8">
+CardWithShading.story = {
+  name: 'Card con ombreggiatura'
+}
+
+export const BigCard = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
       <Card space className="card-bg card-big">
         <CardBody>
@@ -189,12 +239,17 @@ const BigCardComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
-const BigCardTagComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+
+BigCard.story = {
+  name: 'Card grande'
+}
+
+export const BigCardExampleTwo = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
       <Card space className="card-bg card-big border-bottom-card">
         <div className="flag-icon"></div>
@@ -218,13 +273,51 @@ const BigCardTagComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
 
-const CardImageComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+BigCardExampleTwo.story = {
+  name: 'Card grande: esempio alternativo'
+}
+
+export const BigCardWithTag = () => (
+  <Row>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card space className="card-bg card-big no-after">
+        <CardBody>
+          <CardTagsHeader date="10/10/2018">
+            <CardTag href="#">Tag</CardTag>
+          </CardTagsHeader>
+          <CardTitle tag="h5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor…
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </CardText>
+          <div className="it-card-footer">
+            <CardSignature>di Federico De Paolis</CardSignature>
+            <Button outline color="primary" size="sm">
+              Action
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
+      {/* end card */}
+    </Col>
+  </Row>
+)
+
+BigCardWithTag.story = {
+  name: 'Card grande con tag, data e call to action'
+}
+
+export const CardWithImageAndDate = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
       <Card className="card-bg card-img no-after">
         <div className="img-responsive-wrapper">
@@ -251,20 +344,71 @@ const CardImageComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
-  </div>
+    </Col>
+  </Row>
 )
+CardWithImageAndDate.story = {
+  name: 'Card con immagine e data calendario'
+}
 
-const SpecialCardComponent = () => (
-  <div className="row">
-    <div className="col-12 col-lg-6">
+export const CardsWithImages = () => (
+  <Row>
+    <Col xs="12" lg="6">
       {/* Start card */}
-      <Card className="card-img no-after special-card" tag="a">
+      <Card className="card-img no-after">
         <div className="img-responsive-wrapper">
           <div className="img-responsive">
             <figure className="img-wrapper">
               <img
-                src="https://via.placeholder.com/174x214/F9F9FE/0066CC/?text=IMMAGINE%20DI%20ESEMPIO"
+                src="https://via.placeholder.com/310x190/0066cc/FFFFFF/?text=IMMAGINE%20DI%20ESEMPIO"
+                title="img title"
+                alt="imagealt"
+              />
+            </figure>
+          </div>
+        </div>
+        <CardBody>
+          <CardTitle tag="h5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor…
+          </CardTitle>
+          <CardReadMore iconName="it-arrow-right" text="Leggi di più" />
+        </CardBody>
+      </Card>
+      {/* end card */}
+    </Col>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card className="card-img no-after">
+        <div className="img-responsive-wrapper">
+          <div className="img-responsive img-responsive-panoramic">
+            <figure className="img-wrapper">
+              <img
+                src="https://via.placeholder.com/310x94/0066cc/FFFFFF/?text=IMMAGINE%20DI%20ESEMPIO"
+                title="img title"
+                alt="imagealt"
+              />
+            </figure>
+          </div>
+        </div>
+        <CardBody>
+          <CardTitle tag="h5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor…
+          </CardTitle>
+          <CardReadMore iconName="it-arrow-right" text="Leggi di più" />
+        </CardBody>
+      </Card>
+      {/* end card */}
+    </Col>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card className="card-img no-after">
+        <div className="img-responsive-wrapper">
+          <div className="img-responsive">
+            <figure className="img-wrapper">
+              <img
+                src="https://via.placeholder.com/310x190/0066cc/FFFFFF/?text=IMMAGINE%20DI%20ESEMPIO"
                 title="img title"
                 alt="imagealt"
               />
@@ -276,6 +420,63 @@ const SpecialCardComponent = () => (
           </div>
         </div>
         <CardBody>
+          <CardTitle tag="h5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor…
+          </CardTitle>
+          <CardReadMore iconName="it-arrow-right" text="Leggi di più" />
+        </CardBody>
+      </Card>
+      {/* end card */}
+    </Col>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card className="card-img no-after">
+        <div className="img-responsive-wrapper">
+          <div className="img-responsive img-responsive-panoramic">
+            <figure className="img-wrapper">
+              <img
+                src="https://via.placeholder.com/310x94/0066cc/FFFFFF/?text=IMMAGINE%20DI%20ESEMPIO"
+                title="img title"
+                alt="imagealt"
+              />
+            </figure>
+            <div className="card-calendar d-flex flex-column justify-content-center">
+              <span className="card-date">31</span>
+              <span className="card-day">novembre</span>
+            </div>
+          </div>
+        </div>
+        <CardBody>
+          <CardTitle tag="h5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor…
+          </CardTitle>
+          <CardReadMore iconName="it-arrow-right" text="Leggi di più" />
+        </CardBody>
+      </Card>
+      {/* end card */}
+    </Col>
+  </Row>
+)
+
+export const SpecialCard = () => (
+  <Row>
+    <Col xs="12" lg="6">
+      {/* Start card */}
+      <Card className="card-img no-after special-card" tag="a">
+        <div className="img-responsive-wrapper">
+          <div className="img-responsive">
+            <figure className="img-wrapper">
+              <img
+                src="https://via.placeholder.com/174x214/F9F9FE/0066CC/?text=IMMAGINE%20DI%20ESEMPIO"
+                title="img title"
+                alt="imagealt"
+              />
+            </figure>
+          </div>
+        </div>
+        <CardBody>
           <CardTagsHeader date="10/10/2018" />
           <CardTitle tag="h5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -284,66 +485,78 @@ const SpecialCardComponent = () => (
         </CardBody>
       </Card>
       {/* end card */}
-    </div>
+    </Col>
+  </Row>
+)
+SpecialCard.story = {
+  name: 'Card speciali'
+}
+
+export const SimpleCardTeaser = () => (
+  <Card teaser>
+    <CardBody>
+      <CardTitle tag="h5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor…
+      </CardTitle>
+      <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </CardText>
+    </CardBody>
+  </Card>
+)
+
+SimpleCardTeaser.story = {
+  name: 'Card teaser'
+}
+
+export const CardTeaserMultiple = () => (
+  <div className="card-wrapper card-teaser-wrapper">
+    <Card teaser noWrapper>
+      <CardBody>
+        <CardTitle tag="h5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor…
+        </CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </CardText>
+      </CardBody>
+    </Card>
+    <Card teaser noWrapper className="rounded shadow">
+      <CardBody>
+        <CardTitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor…
+        </CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </CardText>
+      </CardBody>
+    </Card>
+    <Card teaser noWrapper className="rounded shadow">
+      <CardBody>
+        <CardCategory iconName="it-clip"></CardCategory>
+        <a href="#">
+          <CardTitle tag="h5">Lorem ipsum dolor sit amet</CardTitle>
+        </a>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </CardText>
+      </CardBody>
+    </Card>
   </div>
 )
 
-export default {
-  title: 'Componenti/Cards'
+CardTeaserMultiple.story = {
+  name: 'Card teaser multiple'
 }
 
-export const _SimpleCard = SimpleCardComponent
-
-export const SimpleCardInMultipleColumns = SimpleCardMultipleColumnsComponent
-
-SimpleCardInMultipleColumns.story = {
-  name: 'Simple Card in multiple columns'
-}
-
-export const _SimpleArticle = SimpleArticleComponent
-
-export const SimpleArticleWithIcon = SimpleArticleDateIconComponent
-
-SimpleArticleWithIcon.story = {
-  name: 'Simple Article with Icon'
-}
-
-export const CardWithIcon = CardIconComponent
-
-CardWithIcon.story = {
-  name: 'Card with icon'
-}
-
-export const CardWithShading = CardShadingComponent
-
-CardWithShading.story = {
-  name: 'Card with shading'
-}
-
-export const _BigCard = BigCardComponent
-
-_BigCard.story = {
-  name: 'Big card'
-}
-
-export const BigCardWithTag = BigCardTagComponent
-
-BigCardWithTag.story = {
-  name: 'Big card with tag'
-}
-
-export const CardWithImage = CardImageComponent
-
-CardWithImage.story = {
-  name: 'Card with image'
-}
-
-export const SpecialCards = SpecialCardComponent
-
-SpecialCards.story = {
-  name: 'Special cards'
-}
-
+// eslint-disable-next-line react/prop-types
 export const _EsempiInterattivi = ({ space, teaser, link, title, text }) => {
   return (
     <Row>
