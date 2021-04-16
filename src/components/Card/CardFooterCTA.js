1da9cc7a299e8b1fff23ce097f297aedab094e54
Luca Errani <luca.errani1@gmail.com>
2021-04-16 08:43:26 +0200

    Revert CartFooterCTA to previous version

--- src/components/Card/CardFooterCTA.js
+++ src/components/Card/CardFooterCTA.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 const propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node
+  className: PropTypes.string
 }
 
 const CardFooterCTA = props => {
-  const { className, children } = props
+  const { className, ...attributes } = props
   const classes = classNames(className, 'it-card-footer')
-  return <div className={classes}>{children}</div>
+  return <div className={classes} {...attributes} />
 }
 
 CardFooterCTA.propTypes = propTypes
