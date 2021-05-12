2899b4c2c7607987ed5af72c43e4882fcc33001c
ewedlund <ewedlund@users.noreply.github.com>
2021-05-12 08:58:19 +0200

    #635 Select wrapper BI style (#707)

--- stories/Form/Select/SelectExamples.js
+++ stories/Form/Select/SelectExamples.js
@@ -1,75 +1,6 @@
 import React, { useState } from 'react'
-import Select, { components } from 'react-select'
 import { boolean } from '@storybook/addon-knobs/react'
-import { Icon } from '../../../src'
-import PropTypes from 'prop-types'
-import './assets/css/select-styles.css'
-
-const SelectContainer = ({ children, ...props }) => {
-  return (
-    <div>
-      <components.SelectContainer {...props}>
-        {children}
-      </components.SelectContainer>
-    </div>
-  )
-}
-
-SelectContainer.propTypes = {
-  children: PropTypes.node
-}
-
-const Option = props => {
-  return (
-    <div className="select-pill text-primary">
-      <components.Option {...props} />
-    </div>
-  )
-}
-const MenuList = ({ children, ...props }) => {
-  return (
-    <div>
-      <components.MenuList {...props}>{children}</components.MenuList>
-    </div>
-  )
-}
-
-MenuList.propTypes = {
-  children: PropTypes.node
-}
-
-const DropdownIndicator = props => {
-  return (
-    <components.DropdownIndicator {...props}>
-      <Icon icon="it-arrow-down-triangle" style={{ ariaHidden: true }} />
-    </components.DropdownIndicator>
-  )
-}
-
-const GroupHeading = props => (
-  <div>
-    <components.GroupHeading {...props} />
-  </div>
-)
-
-const CustomClearText = () => 'Annulla'
-
-const ClearIndicator = props => {
-  const {
-    children = <CustomClearText />,
-    innerProps: { ref, ...restInnerProps }
-  } = props
-  return (
-    <div className="select-pill text-primary" {...restInnerProps} ref={ref}>
-      <div style={{ padding: '0px 5px' }}>{children}</div>
-    </div>
-  )
-}
-
-ClearIndicator.propTypes = {
-  children: PropTypes.node,
-  innerProps: PropTypes.object
-}
+import { Select } from '../../../src'
 
 const defaultOptions = [
   { value: 'Value 1', label: 'Lorem ipsum dolor sit amet' },
@@ -115,17 +46,11 @@ export const SelectExampleClassic = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleClassic">Etichetta di esempio</label>
       <Select
-        components={{
-          Option,
-          DropdownIndicator,
-          IndicatorSeparator: null
-        }}
         id="selectExampleClassic"
         onChange={handleChange}
         options={defaultOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -139,18 +64,12 @@ export const SelectExampleDisabled = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleDisabled">Etichetta di esempio</label>
       <Select
-        components={{
-          Option,
-          DropdownIndicator,
-          IndicatorSeparator: null
-        }}
         id="selectExampleDisabled"
         isDisabled={true}
         onChange={handleChange}
         options={defaultOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -164,19 +83,12 @@ export const SelectExampleReset = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleReset">Etichetta di esempio</label>
       <Select
-        components={{
-          Option,
-          DropdownIndicator,
-          ClearIndicator,
-          IndicatorSeparator: null
-        }}
         id="selectExampleReset"
         isClearable={true}
         onChange={handleChange}
         options={defaultOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -190,18 +102,12 @@ export const SelectExampleSearch = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleSearch">Etichetta di esempio</label>
       <Select
-        components={{
-          Option,
-          DropdownIndicator,
-          IndicatorSeparator: null
-        }}
         id="selectExampleSearch"
         isSearchable={true}
         onChange={handleChange}
         options={defaultOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -215,20 +121,12 @@ export const SelectExampleMulti = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleMulti">Etichetta di esempio</label>
       <Select
-        components={{
-          MenuList,
-          Option,
-          SelectContainer,
-          DropdownIndicator,
-          IndicatorSeparator: null
-        }}
         id="selectExampleMulti"
         isMulti={true}
         onChange={handleChange}
         options={defaultOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -242,19 +140,11 @@ export const SelectExampleGroups = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleGroups">Etichetta di esempio</label>
       <Select
-        components={{
-          MenuList,
-          Option,
-          DropdownIndicator,
-          GroupHeading,
-          IndicatorSeparator: null
-        }}
         id="selectExampleGroups"
         onChange={handleChange}
         options={groupedOptions}
         placeholder="Scegli una opzione"
         aria-label="Scegli una opzione"
-        classNamePrefix="react-select"
       />
     </div>
   )
@@ -280,15 +170,6 @@ export const SelectExampleInteractive = () => {
     <div className="bootstrap-select-wrapper">
       <label htmlFor="selectExampleInteractive">Etichetta di esempio</label>
       <Select
-        components={{
-          MenuList,
-          Option,
-          SelectContainer,
-          DropdownIndicator,
-          ClearIndicator,
-          GroupHeading,
-          IndicatorSeparator: null
-        }}
         id="selectExampleInteractive"
         onChange={handleChange}
         options={options}
@@ -297,8 +178,8 @@ export const SelectExampleInteractive = () => {
         isSearchable={!!search}
         isMulti={!!multi}
         isClearable={!!reset}
+        clearText="Cancella selezione"
         aria-label="Scegli una opzione"
-        classNamePrefix={'react-select'}
       />
     </div>
   )
