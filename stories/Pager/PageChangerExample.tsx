0d1cb1ee5d61d9c23f8603d0c8adf0bfcaa55eb0
Sanyam Dogra <sdlord07@gmail.com>
2021-05-13 21:12:55 +0530

    Ported Page changer to hooks + minor refactor

--- stories/Pager/PageChangerExample.tsx
+++ stories/Pager/PageChangerExample.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Dropdown,
   DropdownToggle,
@@ -7,40 +7,31 @@ import {
   LinkListItem
 } from '../../src';
 
-class PageChangerExample extends React.Component {
-  state = {
-    dropdownOpen: false
-  };
-
-  toggle = () => {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  };
-
-  render() {
-    return (
-      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle
-          tag='a'
-          id='pagerChanger'
-          className='btn btn-dropdown'
-          caret
-        >
-          pag.10
-        </DropdownToggle>
-        <DropdownMenu>
-          <LinkList>
-            <LinkListItem active>pag. 10</LinkListItem>
-            <LinkListItem>pag. 20</LinkListItem>
-            <LinkListItem>pag. 30</LinkListItem>
-            <LinkListItem>pag. 40</LinkListItem>
-            <LinkListItem>pag. 50</LinkListItem>
-          </LinkList>
-        </DropdownMenu>
-      </Dropdown>
-    );
-  }
-}
-
+const PageChangerExample = () => {
+  const [dropdownToggle, setDropdownToggle] = useState(false);
+  return (
+    <Dropdown
+      isOpen={dropdownToggle}
+      toggle={() => setDropdownToggle(!dropdownToggle)}
+    >
+      <DropdownToggle
+        tag='a'
+        id='pagerChanger'
+        className='btn btn-dropdown'
+        caret
+      >
+        pag.10
+      </DropdownToggle>
+      <DropdownMenu>
+        <LinkList>
+          <LinkListItem active>pag. 10</LinkListItem>
+          <LinkListItem>pag. 20</LinkListItem>
+          <LinkListItem>pag. 30</LinkListItem>
+          <LinkListItem>pag. 40</LinkListItem>
+          <LinkListItem>pag. 50</LinkListItem>
+        </LinkList>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 export default PageChangerExample;
