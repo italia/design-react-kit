d877b6d1c70e77777952e91639e01ca440b91606
dej611 <dej611@gmail.com>
2021-09-26 22:34:02 +0200

    :sparkles: Implement example app for treeshake

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,23 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import 'typeface-titillium-web';
+import 'typeface-roboto-mono';
+import 'typeface-lora';
+import Homepage from './homepage';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Homepage
+        townName='Comune di Bugliano'
+        townTagLine=''
+        theme='default'
+        logo={
+          'https://comunedibuglianohome.files.wordpress.com/2020/05/cropped-stemma-comune-2.jpg?w=200'
+        }
+      />
     </div>
   );
 };
