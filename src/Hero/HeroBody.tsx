63fd72e218e7596cc4b3b242f3a2c24c7b9b43c5
dej611 <dej611@gmail.com>
2021-10-02 16:50:45 +0200

    :wrench: Move class to specific component

--- src/Hero/HeroBody.tsx
+++ src/Hero/HeroBody.tsx
@@ -2,18 +2,10 @@ import React, { FC, HTMLAttributes } from 'react';
 import classname from 'classnames';
 import { Col, Container, Row } from 'reactstrap';
 
-export interface HeroBodyProps extends HTMLAttributes<HTMLElement> {
-  dark?: boolean;
-}
+export interface HeroBodyProps extends HTMLAttributes<HTMLElement> {}
 
-export const HeroBody: FC<HeroBodyProps> = ({
-  dark = true,
-  children,
-  className
-}) => {
-  const classes = classname('it-hero-text-wrapper', className, {
-    'bg-dark': dark
-  });
+export const HeroBody: FC<HeroBodyProps> = ({ children, className }) => {
+  const classes = classname('it-hero-text-wrapper', className);
   return (
     <Container>
       <Row>
