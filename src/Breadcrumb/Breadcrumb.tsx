f8795004031159a474695e41876c32f4e2256299
Mario Ravalli <mario@raval.li>
2021-10-25 11:10:23 +0200

    Added default props for compatibility  with source component; Fixed documentation;

--- src/Breadcrumb/Breadcrumb.tsx
+++ src/Breadcrumb/Breadcrumb.tsx
@@ -1,24 +1,37 @@
-import React, { FC, HTMLAttributes } from 'react';
-import { Breadcrumb as BreadcrumbBase } from 'reactstrap';
+import React, { ElementType, FC, HTMLAttributes } from 'react';
+import { Breadcrumb as BreadcrumbBase, CSSModule } from 'reactstrap';
 import classNames from 'classnames';
 
 export interface BreadcrumbProps extends HTMLAttributes<HTMLDivElement> {
+  /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
+  tag?: ElementType;
+  /** Utilizzarlo in caso di utilizzo di componenti personalizzati per la l'elemento lista interno */
+  listTag?: ElementType;
+  /** Classi aggiuntive da usare per il componente Nav */
+  className?: string;
+  /** Classi aggiuntive da usare per il componente di lista interno */
+  listClassName?: string;
+  /** Oggetto contenente la nuova mappatura per le classi CSS. */
+  cssModule?: CSSModule;
   /** Indica se si vuole la versione con fondo scuro */
   dark?: boolean;
   /** Etichetta significativa per descrivere il tipo di navigazione */
-  ariaLabel?: string;
-  /** Classi aggiuntive da usare per il componente Nav */
-  className?: string;
+  'aria-label'?: string;
 }
 
 export const Breadcrumb: FC<BreadcrumbProps> = ({
-  dark,
-  ariaLabel = 'breadcrumb',
+  tag,
+  listTag,
   className,
+  listClassName,
+  cssModule,
+  dark,
+  'aria-label': label,
   ...attributes
 }) => {
   const classes = classNames(className, 'breadcrumb-container');
   const listClasses = dark ? 'dark' : '';
+  const ariaLabel = label ?? 'breadcrumb';
   return (
     <BreadcrumbBase
       className={classes}
