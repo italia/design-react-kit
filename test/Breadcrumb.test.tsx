71b7d723246ec8b9f9e8d8e523b234dfb5eff7e7
Mario Ravalli <mario@raval.li>
2021-10-28 19:21:44 +0200

    Fix Breadcrumb wrapping issue (#770)
    Co-authored-by: Marco Liberati <dej611@users.noreply.github.com>

---
+++ test/Breadcrumb.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Breadcrumb } from '../src';
+
+describe('Breadcrumb component', () => {
+  it('should work as container for children elements', () => {
+    render(<Breadcrumb>Content</Breadcrumb>);
+
+    expect(screen.getByText('Content')).not.toBe(null);
+  });
+
+  it('should append the passed className to the container', () => {
+    const { container } = render(
+      <Breadcrumb className='myclass'>Content</Breadcrumb>
+    );
+
+    expect(container.firstChild).toHaveClass('myclass');
+  });
+
+  it('should add class dark to first ol element when add this breadcrumb prop', () => {
+    const { container } = render(<Breadcrumb dark>Content</Breadcrumb>);
+
+    expect(container.getElementsByClassName('dark').length).toBe(1);
+  });
+});
