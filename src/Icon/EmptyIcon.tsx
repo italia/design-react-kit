ccd106b1c3061e40e9ece905858c98f7ed062060
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-10-11 11:30:55 +0200

    Icon(EmptyIcon): addapting the component at the same way as asset Icons

--- src/Icon/EmptyIcon.tsx
+++ src/Icon/EmptyIcon.tsx
@@ -1,8 +1,15 @@
 import React, { FC, SVGProps } from 'react';
-
-export const EmptyIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+export const EmptyIcon: FC<SVGProps<SVGSVGElement> & SVGRProps> = ({
+  title,
+  titleId,
+  ...props
+}) => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...props}>
-    <title>Empty icon</title>
+    {title ? <title id={titleId}>{title}</title> : null}
     <path fill='none' d='M0 0h24v24H0z' />
   </svg>
 );
