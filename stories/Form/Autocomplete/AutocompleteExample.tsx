51fe319cf6c1b9ee90e73fd8fc467775b1da224e
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-10-19 09:20:32 +0200

    Fixing tests messages (#874)
    * Progress: removing a console.error log during test execution
    
    * AutocompleteExample: removing a waring during test execution
    
    `error TS7006: Parameter 'props' implicitly has an 'any' type`
    
    * tests: upgrading tslib to remove a deprecation warning log during execution
    
    `yarn add -D tslib`
    
    ```
    (node:51045) [DEP0148] DeprecationWarning: Use of deprecated folder mapping "./" in the "exports" field module resolution of the package at /Users/denny/git/design-react-kit/node_modules/tslib/package.json.
    Update this package.json to use a subpath pattern like "./*".
    (Use `node --trace-deprecation ...` to show where the warning was created)
    ```
    
    * upgrading ts-jest and explicitly adding jest devDependency as required
    
    - `yarn add -D ts-jest@26.4.1`
    
        to remove warn log
    
        ```
        ts-jest[versions] (WARN) Version 4.2.4 of typescript installed has not been tested with ts-jest. If you're experiencing issues, consider using a supported version (>=3.4.0 <4.0.0). Please do not report issues in ts-jest if you are using unsupported versions.
        ```
    
    `yarn add -D jest@^26`
    
        to remove warn log
    
        ```
        ts-jest[versions] (WARN) Version 25.5.4 of jest installed has not been tested with ts-jest. If you're experiencing issues, consider using a supported version (>=26.0.0 <27.0.0-0). Please do not report issues in ts-jest if you are using unsupported versions.
        ```

--- stories/Form/Autocomplete/AutocompleteExample.tsx
+++ stories/Form/Autocomplete/AutocompleteExample.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { components, InputProps } from 'react-select';
+import { components, IndicatorProps, InputProps } from 'react-select';
 import './assets/css/autocomplete-styles.css';
 import { Icon } from '../../../src';
 
-export const DropdownIndicator = (props) => {
+export const DropdownIndicator = (props: IndicatorProps<any, boolean, any>) => {
   return (
     <components.DropdownIndicator {...props}>
       <span style={{ padding: '0px 5px' }} aria-hidden='true'>
