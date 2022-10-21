ed3e00fd0be8e69a3e97b26813b1fea191853367
dej611 <dej611@gmail.com>
2022-10-21 09:53:33 +0200

    :bug: Revisit importing script and add testing exception list

---
+++ scripts/import_svgs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Import icons from BI and make them TS React components
+echo 'Importing SVGs...'
+
+npx svgr node_modules/bootstrap-italia/src/svg | tee src/Icon/assets/index.ts
+
+echo 'Linting new components...'
+
+npx eslint src/Icon/assets/*.ts* --fix
+
+echo 'Reconfigure tests for new icons...'
+# Find out what SVGs have no title tag and print them into a txt file used for testing
+find node_modules/bootstrap-italia/src/svg -iname "*.svg" -print0 -exec grep -Li "title" {} + | xargs -L 1 basename > ./test/icons-with-no-title.txt
