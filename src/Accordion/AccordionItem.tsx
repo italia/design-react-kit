a6502a272bb368a144890b4c36feed770cf4b526
sabato-galasso <sabatogalasso90@gmail.com>
2023-01-25 17:29:26 +0100

    Update src/Accordion/AccordionItem.tsx
    Co-authored-by: Marco Liberati <dej611@users.noreply.github.com>

--- src/Accordion/AccordionItem.tsx
+++ src/Accordion/AccordionItem.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 export interface AccordionItemProps {
   /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
   tag?: ElementType;
-  /** Classi aggiuntive da usare per il componente Badge */
+  /** Classi aggiuntive da usare per il componente */
   className?: string;
 }
 
