866ad15b1e013287a4a8ef349b87b886db712ba7
sabato-galasso <sabatogalasso90@gmail.com>
2023-02-08 11:59:11 +0100

    removed unused props

--- src/Input/utils.tsx
+++ src/Input/utils.tsx
@@ -40,7 +40,7 @@ export function getFormControlClass(
 }
 
 export function getInfoTextControlClass(
-  { valid, invalid }: ValidationProps,
+  { invalid }: ValidationProps,
   cssModule?: CSSModule
 ) {
   return mapToCssModules(
