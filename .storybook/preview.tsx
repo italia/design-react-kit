27c1f33ed857ad5b7f1bd949c3f7b5b5fc74467c
sabato-galasso <sabatogalasso90@gmail.com>
2023-02-13 14:12:41 +0100

    Fix PR #885

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,19 +1,28 @@
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 import { theme } from './theme';
-import {FontLoader} from '../src';
+import { FontLoader } from '../src';
 
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
 import '../assets/css/fonts.css';
 import '../assets/css/storybook-fixes.css';
 
 const order = {
-  'documentazione': 1,
-  'templates': 100,
-  'componenti': 1000,
+  documentazione: 1,
+  templates: 100,
+  componenti: 1000
 };
 
-const prioritySections = ['welcome-page', 'organizzare', 'menu', 'componenti', 'form', 'utilities', 'domande-frequenti-page', 'changelog-page', 'migrazione-da-v1-page'];
-const priorityPages = ["introduzione"];
+const prioritySections = [
+  'welcome-page',
+  'organizzare',
+  'menu',
+  'componenti',
+  'form',
+  'utilities',
+  'domande-frequenti-page',
+  'changelog-page'
+];
+const priorityPages = ['introduzione'];
 
 const customStyle = {
   position: 'relative',
@@ -21,7 +30,7 @@ const customStyle = {
   margin: '0',
   padding: '48px 32px',
   textAlign: 'initial'
-} as CSSProperties
+} as CSSProperties;
 
 type MainSection = keyof typeof order;
 
@@ -36,46 +45,57 @@ export const parameters = {
       const [aSection, ...aSubSections] = aGroups.split('-');
       const [bSection, ...bSubSections] = bGroups.split('-');
 
-      const aFullStoryName = `${aSubSections.join('-')}-${aStory}`
-      const bFullStoryName = `${bSubSections.join('-')}-${bStory}`
+      const aFullStoryName = `${aSubSections.join('-')}-${aStory}`;
+      const bFullStoryName = `${bSubSections.join('-')}-${bStory}`;
       // Sort by Section
-      const groupScore = (order[aSection as MainSection] - order[bSection as MainSection]);
-
+      const groupScore =
+        order[aSection as MainSection] - order[bSection as MainSection];
 
       // if pages have custom sort, compare them only if from the same section
       if (!groupScore) {
-        const pageASectionIndex = prioritySections.indexOf(aFullStoryName) + 1 || prioritySections.indexOf(aSubSections[0]) + 1;
-        const pageBSectionIndex = prioritySections.indexOf(bFullStoryName) + 1 || prioritySections.indexOf(bSubSections[0]) + 1;
+        const pageASectionIndex =
+          prioritySections.indexOf(aFullStoryName) + 1 ||
+          prioritySections.indexOf(aSubSections[0]) + 1;
+        const pageBSectionIndex =
+          prioritySections.indexOf(bFullStoryName) + 1 ||
+          prioritySections.indexOf(bSubSections[0]) + 1;
         if (pageASectionIndex || pageBSectionIndex) {
           const sectionScore = pageASectionIndex - pageBSectionIndex;
 
           if (!sectionScore) {
-            const pageAIndex = priorityPages.indexOf(aSubSections[aSubSections.length - 1]) + 1;
-            const pageBIndex = priorityPages.indexOf(bSubSections[bSubSections.length - 1]) + 1;
+            const pageAIndex =
+              priorityPages.indexOf(aSubSections[aSubSections.length - 1]) + 1;
+            const pageBIndex =
+              priorityPages.indexOf(bSubSections[bSubSections.length - 1]) + 1;
             if (pageAIndex || pageBIndex) {
               return pageBIndex - pageAIndex;
             }
-            return (aFullStoryName.localeCompare(bFullStoryName));
+            return aFullStoryName.localeCompare(bFullStoryName);
           }
           return sectionScore;
         }
       }
 
       // and lexycographic order of the story not mapped
-      return groupScore + (aFullStoryName.localeCompare(bFullStoryName))
+      return groupScore + aFullStoryName.localeCompare(bFullStoryName);
     },
     /**
      * where to show the addon panel
      * @type {('bottom'|'right')}
      */
     panelPosition: 'right',
-    previewTabs: { 'storybook/docs/panel': { index: -1 } },
+    previewTabs: { 'storybook/docs/panel': { index: -1 } }
   },
   docs: {
     theme
-  },
+  }
 };
 
 export const decorators = [
-  (Story: Function) => <div style={customStyle}><FontLoader /><Story/></div>
-]
+  (Story: Function) => (
+    <div style={customStyle}>
+      <FontLoader />
+      <Story />
+    </div>
+  )
+];
---
+++ src/Icon/assets/ItFileVideo.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+export const component = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-labelledby={titleId}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <g clipPath='url(#a)'>
+        <path
+          clipRule='evenodd'
+          d='M6.5 2h8.2L19 6.3v14.2a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 5 20.5v-17A1.5 1.5 0 0 1 6.5 2Zm10.8 4L15 3.7v1.8a.5.5 0 0 0 .5.5h1.8Zm.2 15h-11a.5.5 0 0 1-.5-.5v-17a.5.5 0 0 1 .5-.5H14v2.5A1.5 1.5 0 0 0 15.5 7H18v13.5a.5.5 0 0 1-.5.5ZM10 15.426v-4.852c0-.445.468-.72.836-.492l3.896 2.427a.585.585 0 0 1 0 .982l-3.896 2.427c-.368.229-.836-.046-.836-.492Z'
+        />
+      </g>
+      <defs>
+        <clipPath id='a'>
+          <path d='M0 0h24v24H0z' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
