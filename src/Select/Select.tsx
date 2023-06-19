7d46fe31604b0e9941afa406fdc3e990a9390344
Federico Turbino <f.turbino@liguriadigitale.it>
2023-06-19 11:59:11 +0200

    docs: add jsdocs

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -2,10 +2,24 @@ import React, { ChangeEvent, ReactElement } from 'react';
 import { Input } from 'reactstrap';
 
 export interface SelectProps {
+  /**
+   * L'identificativo univoco del componente
+   */
   id: string;
+  /**
+   * Se `true` determina la disabilitazione del campo
+   */
   disabled?: boolean;
+  /**
+   * L'etichetta del campo
+   */
   label: string;
   children: ReactElement<'option'>[];
+  /**
+   * Gestisce il cambio del valore selezionato
+   * @param selectedValue
+   * @returns
+   */
   handleChange: (selectedValue: string) => void;
 }
 
