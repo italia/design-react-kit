51d49f54ea59def7d83d3fdf3230bcaf5e3ba0e2
Federico Turbino <f.turbino@liguriadigitale.it>
2023-06-26 17:29:06 +0200

    refactor: use directly option elements

--- stories/Form/Select.stories.tsx
+++ stories/Form/Select.stories.tsx
@@ -2,42 +2,6 @@ import React, { useState } from 'react';
 
 import { Select } from '../../src';
 
-const defaultOptions = [
-  { value: 'Value 1', label: 'Lorem ipsum dolor sit amet' },
-  { value: 'Value 2', label: 'Duis vestibulum eleifend libero' },
-  { value: 'Value 3', label: 'Phasellus pretium orci sed odio tempus' },
-  {
-    value: 'Value 4',
-    label: 'Vestibulum bibendum ex vel augue porttitor sodales'
-  },
-  { value: 'Value 5', label: 'Praesent quis elementum turpis' }
-];
-
-const groupOneOptions = [
-  { value: 'Value 1', label: 'Lorem ipsum dolor sit amet' },
-  { value: 'Value 2', label: 'Duis vestibulum eleifend libero' },
-  { value: 'Value 3', label: 'Phasellus pretium orci sed odio tempus' }
-];
-
-const groupTwoOptions = [
-  {
-    value: 'Value 4',
-    label: 'Vestibulum bibendum ex vel augue porttitor sodales'
-  },
-  { value: 'Value 5', label: 'Praesent quis elementum turpis' }
-];
-
-const groupedOptions = [
-  {
-    label: 'Gruppo 1',
-    options: groupOneOptions
-  },
-  {
-    label: 'Gruppo 2',
-    options: groupTwoOptions
-  }
-];
-
 export default {
   title: 'Componenti/Form/Select'
 };
@@ -53,11 +17,14 @@ export const SelectClassica = () => {
       label='Etichetta di esempio'
       handleChange={handleChange}
     >
-      {defaultOptions.map((opt, i) => (
-        <option label={opt.label} key={i}>
-          {opt.value}
-        </option>
-      ))}
+      <option value=''>Scegli un'opzione</option>
+      <option value='Value 1'>Lorem ipsum dolor sit amet</option>
+      <option value='Value 2'>Duis vestibulum eleifend libero</option>
+      <option value='Value 3'>Phasellus pretium orci sed odio tempus</option>
+      <option value='Value 4'>
+        Vestibulum bibendum ex vel augue porttitor sodales
+      </option>
+      <option value='Value 5'>Praesent quis elementum turpis</option>
     </Select>
   );
 };
@@ -76,11 +43,14 @@ export const SelectDisabilitata = () => {
       handleChange={handleChange}
       disabled
     >
-      {defaultOptions.map((opt, i) => (
-        <option label={opt.label} key={i}>
-          {opt.value}
-        </option>
-      ))}
+      <option value=''>Scegli un'opzione</option>
+      <option value='Value 1'>Lorem ipsum dolor sit amet</option>
+      <option value='Value 2'>Duis vestibulum eleifend libero</option>
+      <option value='Value 3'>Phasellus pretium orci sed odio tempus</option>
+      <option value='Value 4'>
+        Vestibulum bibendum ex vel augue porttitor sodales
+      </option>
+      <option value='Value 5'>Praesent quis elementum turpis</option>
     </Select>
   );
 };
@@ -98,13 +68,18 @@ export const SelectConGruppi = () => {
       label='Etichetta di esempio'
       handleChange={handleChange}
     >
-      {groupedOptions.map((grp, i) => (
-        <optgroup label={grp.label}>
-          {grp.options.map((opt, j) => (
-            <option value={opt.value}>{opt.label}</option>
-          ))}
-        </optgroup>
-      ))}
+      <option value=''>Scegli un'opzione</option>
+      <optgroup label='Gruppo 1'>
+        <option value='Value 1'>Lorem ipsum dolor sit amet</option>
+        <option value='Value 2'>Duis vestibulum eleifend libero</option>
+        <option value='Value 3'>Phasellus pretium orci sed odio tempus</option>
+      </optgroup>
+      <optgroup label='Gruppo 2'>
+        <option value='Value 4'>
+          Vestibulum bibendum ex vel augue porttitor sodales
+        </option>
+        <option value='Value 5'>Praesent quis elementum turpis</option>
+      </optgroup>
     </Select>
   );
 };
