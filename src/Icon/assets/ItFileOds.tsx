70529bd25a559ae01f9761c6ac8678f8e3ae30f2
Andrea Stagi <stagi.andrea@gmail.com>
2023-12-06 22:09:41 +0100

    fix: icons

--- src/Icon/assets/ItFileOds.tsx
+++ src/Icon/assets/ItFileOds.tsx
@@ -10,7 +10,6 @@ export const component = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> 
       width={24}
       height={24}
       viewBox='0 0 24 24'
-      fill='none'
       xmlns='http://www.w3.org/2000/svg'
       aria-labelledby={titleId}
       {...props}
