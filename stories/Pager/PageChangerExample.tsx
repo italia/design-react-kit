fae1b95c872b278d3073811a55e0ac195cfef8c4
Andrea Stagi <stagi.andrea@gmail.com>
2024-01-12 18:47:36 +0100

    fix: pagination dropdown

--- stories/Pager/PageChangerExample.tsx
+++ stories/Pager/PageChangerExample.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, LinkList, LinkListItem } from '../../src';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, LinkList, LinkListItem, Icon } from '../../src';
 
 const PageChangerExample = () => {
   const [dropdownToggle, setDropdownToggle] = useState(false);
   return (
-    <Dropdown isOpen={dropdownToggle} toggle={() => setDropdownToggle(!dropdownToggle)}>
-      <DropdownToggle tag='a' id='pagerChanger' className='btn btn-dropdown' caret>
-        pag.10
+    <UncontrolledDropdown isOpen={dropdownToggle} toggle={() => setDropdownToggle(!dropdownToggle)}>
+      <DropdownToggle
+        id='pagerChanger'
+        className={`btn btn-dropdown dropdown-toggle`}
+        tag='button'
+        data-toggle='dropdown'
+        aria-haspopup
+        aria-expanded='false'
+        aria-label='Salta alla pagina'
+        caret
+      >
+        10/pagina
+        <Icon className='icon icon-primary icon-sm' icon='it-expand' />
       </DropdownToggle>
       <DropdownMenu>
         <LinkList>
@@ -17,7 +27,7 @@ const PageChangerExample = () => {
           <LinkListItem>pag. 50</LinkListItem>
         </LinkList>
       </DropdownMenu>
-    </Dropdown>
+    </UncontrolledDropdown>
   );
 };
 export default PageChangerExample;
