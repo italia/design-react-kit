ac420f0c757c63caf13a6b7ce5ae20560943b0b3
Andrea Stagi <stagi.andrea@gmail.com>
2024-01-24 21:59:07 +0100

    fix: dropdown nav item

--- stories/Header/NavHeader/basic.tsx
+++ stories/Header/NavHeader/basic.tsx
@@ -59,34 +59,28 @@ const NavHeader = ({ theme, isOpen }: HeaderNavProps) => {
               <NavItem>
                 <NavLink href='#'>Link 4</NavLink>
               </NavItem>
-              <NavItem>
-                <UncontrolledDropdown nav tag='div'>
-                  <DropdownToggle nav caret>
-                    Dropdown item
-                    <Icon icon='it-expand' />
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <LinkList>
-                      <LinkListItem header tag='h3' id='heading-es-1'>
-                        Heading
-                      </LinkListItem>
-                      <LinkListItem tag={DropdownItem} href='#'>
-                        <span>Link list 1</span>
-                      </LinkListItem>
-                      <LinkListItem tag={DropdownItem} href='#'>
-                        <span>Link list 2</span>
-                      </LinkListItem>
-                      <LinkListItem tag={DropdownItem} href='#'>
-                        <span>Link list 3</span>
-                      </LinkListItem>
-                      <LinkListItem divider />
-                      <LinkListItem tag={DropdownItem} href='#'>
-                        <span>Link list 4</span>
-                      </LinkListItem>
-                    </LinkList>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </NavItem>
+              <UncontrolledDropdown nav tag='li'>
+                <DropdownToggle nav caret role='button'>
+                  <span>Menu Dropdown</span>
+                  <Icon icon='it-expand' size='xs' />
+                </DropdownToggle>
+                <DropdownMenu>
+                  <LinkList>
+                    <LinkListItem tag={DropdownItem} href='#'>
+                      <span>Link list 1</span>
+                    </LinkListItem>
+                    <LinkListItem tag={DropdownItem} href='#'>
+                      <span>Link list 2</span>
+                    </LinkListItem>
+                    <LinkListItem tag={DropdownItem} href='#'>
+                      <span>Link list 3</span>
+                    </LinkListItem>
+                    <LinkListItem tag={DropdownItem} href='#'>
+                      <span>Link list 4</span>
+                    </LinkListItem>
+                  </LinkList>
+                </DropdownMenu>
+              </UncontrolledDropdown>
               <NavItem>
                 <NavLink href='#' disabled>
                   Megamenu label
