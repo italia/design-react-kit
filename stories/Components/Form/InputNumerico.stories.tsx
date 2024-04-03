a4849e5c0fd8f2f89f6cf0c62274e281be6ee2cc
Virtute90 <matteo.ave@gmail.com>
2024-04-03 20:56:44 +0000

    Input numerico, select e forward hooks ok

--- stories/Components/Form/InputNumerico.stories.tsx
+++ stories/Components/Form/InputNumerico.stories.tsx
@@ -11,74 +11,84 @@ export default meta;
 
 type Story = StoryObj<typeof Input>;
 
+const InputNumericoDimensionamentoHooks = () => {
+    const [value, setValue] = useState("100");
+    const [value50, setValue50] = useState("100");
+    return (
+        <>
+            <div className="w-100">
+                <Input
+                    id="example-w100"
+                    type="number"
+                    label="Input Number inserito in una colonna a tutta larghezza"
+                    incrementLabel="Aumenta il valore  di 1"
+                    decrementLabel="Diminuisci il valore di 1"
+                    value={value}
+                    onChange={(ev) => {
+                        setValue(ev.target.value);
+                    }}
+                />
+            </div>
+            <div className="w-50 mt-5">
+                <Input
+                    id="example-w50"
+                    type="number"
+                    label="Input Number inserito in una colonna di larghezza 50%"
+                    incrementLabel="Aumenta il valore  di 1"
+                    decrementLabel="Diminuisci il valore di 1"
+                    value={value50}
+                    onChange={(ev) => {
+                        setValue50(ev.target.value);
+                    }}
+                />
+            </div>
+        </>
+    );
+
+};
+
 export const InputNumericoDimensionamento: Story = {
     render: () => {
-        const [value, setValue] = useState("100");
-        const [value50, setValue50] = useState("100");
-        return (
-            <>
-                <div className="w-100">
-                    <Input
-                        id="example-w100"
-                        type="number"
-                        label="Input Number inserito in una colonna a tutta larghezza"
-                        incrementLabel="Aumenta il valore  di 1"
-                        decrementLabel="Diminuisci il valore di 1"
-                        value={value}
-                        onChange={(ev) => {
-                            setValue(ev.target.value);
-                        }}
-                    />
-                </div>
-                <div className="w-50 mt-5">
-                    <Input
-                        id="example-w50"
-                        type="number"
-                        label="Input Number inserito in una colonna di larghezza 50%"
-                        incrementLabel="Aumenta il valore  di 1"
-                        decrementLabel="Diminuisci il valore di 1"
-                        value={value50}
-                        onChange={(ev) => {
-                            setValue50(ev.target.value);
-                        }}
-                    />
-                </div>
-            </>
-        );
+        return <InputNumericoDimensionamentoHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
+}
+
+const InputNumericoStepsHooks = () => {
+    const [value, setValue] = useState("100");
+    return (
+        <Input
+            id="example-steps"
+            type="number"
+            label="Min, Max & Step"
+            incrementLabel="Aumenta il valore  di 500"
+            decrementLabel="Diminuisci il valore di 500"
+            value={value}
+            min={-2000}
+            max={15000}
+            step={500}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
 };
 
 export const InputNumericoSteps: Story = {
     render: () => {
-        const [value, setValue] = useState("100");
-        return (
-            <Input
-                id="example-steps"
-                type="number"
-                label="Min, Max & Step"
-                incrementLabel="Aumenta il valore  di 500"
-                decrementLabel="Diminuisci il valore di 500"
-                value={value}
-                min={-2000}
-                max={15000}
-                step={500}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputNumericoStepsHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
-};
+}
+
 
 export const InputNumericoDisabilitato: Story = {
     render: () => {
@@ -86,87 +96,100 @@ export const InputNumericoDisabilitato: Story = {
     },
 };
 
+const InputNumericoValutaHooks = () => {
+    const [value, setValue] = useState("100");
+
+    return (
+        <Input
+            id="example-currency"
+            addonText="€"
+            type="currency"
+            label="Currency"
+            incrementLabel="Aumenta il valore di 1 euro"
+            decrementLabel="Diminuisci il valore di 1 euro"
+            value={value}
+            step="any"
+            min={3.5}
+            max={100}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
+};
+
 export const InputNumericoValuta: Story = {
     render: () => {
-        const [value, setValue] = useState("100");
-
-        return (
-            <Input
-                id="example-currency"
-                addonText="€"
-                type="currency"
-                label="Currency"
-                incrementLabel="Aumenta il valore di 1 euro"
-                decrementLabel="Diminuisci il valore di 1 euro"
-                value={value}
-                step="any"
-                min={3.5}
-                max={100}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputNumericoValutaHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
+}
+
+
+const InputNumericoPercentualeHooks = () => {
+    const [value, setValue] = useState("100");
+
+    return (
+        <Input
+            id="example-percentage"
+            addonText="%"
+            type="percentage"
+            label="Percentage"
+            incrementLabel="Aumenta il valore in percentuale di 1"
+            decrementLabel="Diminuisci il valore in percentuale di 1"
+            value={value}
+            min={0}
+            max={100}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
 };
 
 export const InputNumericoPercentuale: Story = {
     render: () => {
-        const [value, setValue] = useState("100");
-
-        return (
-            <Input
-                id="example-percentage"
-                addonText="%"
-                type="percentage"
-                label="Percentage"
-                incrementLabel="Aumenta il valore in percentuale di 1"
-                decrementLabel="Diminuisci il valore in percentuale di 1"
-                value={value}
-                min={0}
-                max={100}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputNumericoPercentualeHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
+}
+
+const InputNumericoRidimensionamentoHooks = () => {
+    const [value, setValue] = useState("100");
+
+    return (
+        <Input
+            id="example-adaptive"
+            type="adaptive"
+            label="Ridimensionamento"
+            incrementLabel="Aumenta il valore  di 1"
+            decrementLabel="Diminuisci il valore di 1"
+            value={value}
+            placeholder={"0"}
+            min={0}
+            max={99999999999}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
 };
 
 export const InputNumericoRidimensionamento: Story = {
     render: () => {
-        const [value, setValue] = useState("100");
-
-        return (
-            <Input
-                id="example-adaptive"
-                type="adaptive"
-                label="Ridimensionamento"
-                incrementLabel="Aumenta il valore  di 1"
-                decrementLabel="Diminuisci il valore di 1"
-                value={value}
-                placeholder={"0"}
-                min={0}
-                max={99999999999}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputNumericoRidimensionamentoHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
-};
+}
--- stories/Components/Form/Select.stories.tsx
+++ stories/Components/Form/Select.stories.tsx
@@ -16,63 +16,78 @@ export default meta;
 
 type Story = StoryObj<typeof Select>;
 
+const SelectClassicaHooks = () => {
+    const [, setValue] = useState<string>();
+
+    const handleChange = (selectedOption: string) => setValue(selectedOption);
+
+    return (
+        <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange}>
+            <option value="">Scegli un'opzione</option>
+            <option value="Value 1">Lorem ipsum dolor sit amet</option>
+            <option value="Value 2">Duis vestibulum eleifend libero</option>
+            <option value="Value 3">Phasellus pretium orci sed odio tempus</option>
+            <option value="Value 4">Vestibulum bibendum ex vel augue porttitor sodales</option>
+            <option value="Value 5">Praesent quis elementum turpis</option>
+        </Select>
+    );
+
+};
+
 export const SelectClassica: Story = {
     render: () => {
-        const [, setValue] = useState<string>();
-
-        const handleChange = (selectedOption: string) => setValue(selectedOption);
-
-        return (
-            <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange}>
-                <option value="">Scegli un'opzione</option>
-                <option value="Value 1">Lorem ipsum dolor sit amet</option>
-                <option value="Value 2">Duis vestibulum eleifend libero</option>
-                <option value="Value 3">Phasellus pretium orci sed odio tempus</option>
-                <option value="Value 4">Vestibulum bibendum ex vel augue porttitor sodales</option>
-                <option value="Value 5">Praesent quis elementum turpis</option>
-            </Select>
-        );
+        return <SelectClassicaHooks />
     }
+}
+
+const SelectDisabilitataHooks = () => {
+    const [, setValue] = useState<string>();
+
+    const handleChange = (selectedOption: string) => setValue(selectedOption);
+
+    return (
+        <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange} disabled>
+            <option value="">Scegli un'opzione</option>
+            <option value="Value 1">Lorem ipsum dolor sit amet</option>
+            <option value="Value 2">Duis vestibulum eleifend libero</option>
+            <option value="Value 3">Phasellus pretium orci sed odio tempus</option>
+            <option value="Value 4">Vestibulum bibendum ex vel augue porttitor sodales</option>
+            <option value="Value 5">Praesent quis elementum turpis</option>
+        </Select>
+    );
+
 };
 
 export const SelectDisabilitata: Story = {
     render: () => {
-        const [, setValue] = useState<string>();
+        return <SelectDisabilitataHooks />
+    }
+}
 
-        const handleChange = (selectedOption: string) => setValue(selectedOption);
+const SelectConGruppiHooks = () => {
+    const [, setValue] = useState<string>();
 
-        return (
-            <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange} disabled>
-                <option value="">Scegli un'opzione</option>
+    const handleChange = (selectedOption: string) => setValue(selectedOption);
+
+    return (
+        <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange}>
+            <option value="">Scegli un'opzione</option>
+            <optgroup label="Gruppo 1">
                 <option value="Value 1">Lorem ipsum dolor sit amet</option>
                 <option value="Value 2">Duis vestibulum eleifend libero</option>
                 <option value="Value 3">Phasellus pretium orci sed odio tempus</option>
+            </optgroup>
+            <optgroup label="Gruppo 2">
                 <option value="Value 4">Vestibulum bibendum ex vel augue porttitor sodales</option>
                 <option value="Value 5">Praesent quis elementum turpis</option>
-            </Select>
-        );
-    }
+            </optgroup>
+        </Select>
+    );
+
 };
 
 export const SelectConGruppi: Story = {
     render: () => {
-        const [, setValue] = useState<string>();
-
-        const handleChange = (selectedOption: string) => setValue(selectedOption);
-
-        return (
-            <Select id="example-reactstrap" label="Etichetta di esempio" onChange={handleChange}>
-                <option value="">Scegli un'opzione</option>
-                <optgroup label="Gruppo 1">
-                    <option value="Value 1">Lorem ipsum dolor sit amet</option>
-                    <option value="Value 2">Duis vestibulum eleifend libero</option>
-                    <option value="Value 3">Phasellus pretium orci sed odio tempus</option>
-                </optgroup>
-                <optgroup label="Gruppo 2">
-                    <option value="Value 4">Vestibulum bibendum ex vel augue porttitor sodales</option>
-                    <option value="Value 5">Praesent quis elementum turpis</option>
-                </optgroup>
-            </Select>
-        );
+        return <SelectConGruppiHooks />
     }
-};
+}
