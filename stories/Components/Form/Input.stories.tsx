c8dadb5128c05471e781235a22b04e52431f9742
Virtute90 <matteo.ave@gmail.com>
2024-04-03 21:01:12 +0000

    Input e validazione hooks ok

--- stories/Components/Form/Input.stories.tsx
+++ stories/Components/Form/Input.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
-//import Autocomplete from "accessible-autocomplete/react"; // Reference to https://www.npmjs.com/package/accessible-autocomplete
+import Autocomplete from "accessible-autocomplete/react"; // Reference to https://www.npmjs.com/package/accessible-autocomplete
 import React, { useState } from "react";
-import { Icon, Input, TextArea } from "../../../src";
+import { FormGroup, Icon, Input, TextArea } from "../../../src";
 
 const meta: Meta<typeof Input> = {
     title: "Documentazione/Form/Input",
@@ -11,7 +11,6 @@ const meta: Meta<typeof Input> = {
 export default meta;
 
 type Story = StoryObj<typeof Input>;
-
 
 export const EsempiDiCampiDiInput: Story = {
     render: () => {
@@ -44,104 +43,109 @@ export const UtilizzoDiPlaceholderELabel: Story = {
     )
 };
 
-export const InputConIconaOBottoni: Story = {
-    render: () => {
-        const [isFocused1, toggleFocus1] = useState(false);
-        const [isFocused2, toggleFocus2] = useState(false);
-        const [isFocused3, toggleFocus3] = useState(false);
+const InputConIconaOBottoniHooks = () => {
+    const [isFocused1, toggleFocus1] = useState(false);
+    const [isFocused2, toggleFocus2] = useState(false);
+    const [isFocused3, toggleFocus3] = useState(false);
 
-        const toggleFocusLabel1 = () => toggleFocus1(true);
-        // @ts-ignore: ignore types for now
-        const toggleBlurLabel1 = (e) => {
-            if (e.target.value === "") {
-                toggleFocus1(!isFocused1);
-            }
-        };
-        const toggleFocusLabel2 = () => toggleFocus2(true);
-        // @ts-ignore: ignore types for now
-        const toggleBlurLabel2 = (e) => {
-            if (e.target.value === "") {
-                toggleFocus2(!isFocused2);
-            }
-        };
-        const toggleFocusLabel3 = () => toggleFocus3(true);
-        // @ts-ignore: ignore types for no}w
-        const toggleBlurLabel3 = (e) => {
-            if (e.target.value === "") {
-                toggleFocus3(!isFocused3);
-            }
-        };
-        return (
-            <div>
-                <div className="form-group">
-                    <div className="input-group">
-                        <span className="input-group-text">
-                            <Icon icon="it-pencil" aria-hidden size="sm" />
-                        </span>
-                        <label htmlFor="input-group-1" className={isFocused1 ? "active" : ""}>
-                            Con Etichetta
-                        </label>
-                        <input
-                            type="text"
-                            className={isFocused1 ? "form-control focus--mouse" : "form-control"}
-                            onFocus={toggleFocusLabel1}
-                            onBlur={toggleBlurLabel1}
-                            id="input-group-1"
-                            name="input-group-1"
-                        />
-                    </div>
+    const toggleFocusLabel1 = () => toggleFocus1(true);
+    // @ts-ignore: ignore types for now
+    const toggleBlurLabel1 = (e) => {
+        if (e.target.value === "") {
+            toggleFocus1(!isFocused1);
+        }
+    };
+    const toggleFocusLabel2 = () => toggleFocus2(true);
+    // @ts-ignore: ignore types for now
+    const toggleBlurLabel2 = (e) => {
+        if (e.target.value === "") {
+            toggleFocus2(!isFocused2);
+        }
+    };
+    const toggleFocusLabel3 = () => toggleFocus3(true);
+    // @ts-ignore: ignore types for no}w
+    const toggleBlurLabel3 = (e) => {
+        if (e.target.value === "") {
+            toggleFocus3(!isFocused3);
+        }
+    };
+    return (
+        <div>
+            <div className="form-group">
+                <div className="input-group">
+                    <span className="input-group-text">
+                        <Icon icon="it-pencil" aria-hidden size="sm" />
+                    </span>
+                    <label htmlFor="input-group-1" className={isFocused1 ? "active" : ""}>
+                        Con Etichetta
+                    </label>
+                    <input
+                        type="text"
+                        className={isFocused1 ? "form-control focus--mouse" : "form-control"}
+                        onFocus={toggleFocusLabel1}
+                        onBlur={toggleBlurLabel1}
+                        id="input-group-1"
+                        name="input-group-1"
+                    />
                 </div>
-                <div className="form-group">
-                    <div className="input-group">
-                        <span className="input-group-text">
-                            <Icon icon="it-pencil" color="danger" aria-hidden size="sm" />
-                        </span>
-                        <label htmlFor="input-group-2" className="active">
-                            Con Etichetta e placeholder
-                        </label>
-                        <input
-                            type="text"
-                            className={isFocused2 ? "form-control focus--mouse" : "form-control"}
-                            onFocus={toggleFocusLabel2}
-                            onBlur={toggleBlurLabel2}
-                            id="input-group-2"
-                            name="input-group-2"
-                            placeholder="Lorem Ipsum"
-                        />
-                    </div>
+            </div>
+            <div className="form-group">
+                <div className="input-group">
+                    <span className="input-group-text">
+                        <Icon icon="it-pencil" color="danger" aria-hidden size="sm" />
+                    </span>
+                    <label htmlFor="input-group-2" className="active">
+                        Con Etichetta e placeholder
+                    </label>
+                    <input
+                        type="text"
+                        className={isFocused2 ? "form-control focus--mouse" : "form-control"}
+                        onFocus={toggleFocusLabel2}
+                        onBlur={toggleBlurLabel2}
+                        id="input-group-2"
+                        name="input-group-2"
+                        placeholder="Lorem Ipsum"
+                    />
                 </div>
-                <div className="form-group">
-                    <div className="input-group">
-                        <span className="input-group-text">
-                            <Icon icon="it-pencil" color="primary" aria-hidden size="sm" />
-                        </span>
-                        <label htmlFor="input-group-3" className={isFocused3 ? "active" : ""}>
-                            Con Etichetta e bottone di tipo primary
-                        </label>
-                        <input
-                            type="text"
-                            className={isFocused3 ? "form-control focus--mouse" : "form-control"}
-                            onFocus={toggleFocusLabel3}
-                            onBlur={toggleBlurLabel3}
-                            id="input-group-3"
-                            name="input-group-3"
-                        />
-                        <div className="input-group-append">
-                            <button className="btn btn-primary" type="button" id="button-3">
-                                Invio
-                            </button>
-                        </div>
+            </div>
+            <div className="form-group">
+                <div className="input-group">
+                    <span className="input-group-text">
+                        <Icon icon="it-pencil" color="primary" aria-hidden size="sm" />
+                    </span>
+                    <label htmlFor="input-group-3" className={isFocused3 ? "active" : ""}>
+                        Con Etichetta e bottone di tipo primary
+                    </label>
+                    <input
+                        type="text"
+                        className={isFocused3 ? "form-control focus--mouse" : "form-control"}
+                        onFocus={toggleFocusLabel3}
+                        onBlur={toggleBlurLabel3}
+                        id="input-group-3"
+                        name="input-group-3"
+                    />
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="button" id="button-3">
+                            Invio
+                        </button>
                     </div>
                 </div>
             </div>
-        );
+        </div>
+    );
+};
+
+export const InputConIconaOBottoni: Story = {
+    render: () => {
+        return <InputConIconaOBottoniHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
-};
+}
+
 
 export const InputPassword: Story = {
     args: {
@@ -177,30 +181,30 @@ export const ReadonlyNormalizzato: Story = {
     }
 }
 
-/* export const _InputAutocompleteConDatiAccessibile: Story = {
+export const _InputAutocompleteConDatiAccessibile: Story = {
     render: () => {
         const multiOptions: { value: string, label: string }[] = [
-    { value: "1", label: "Abruzzo" },
-    { value: "2", label: "Basilicata" },
-    { value: "3", label: "Calabria" },
-    { value: "4", label: "Campania" },
-    { value: "5", label: "Emilia Romagna" },
-    { value: "6", label: "Friuli Venezia Giulia" },
-    { value: "7", label: "Lazio" },
-    { value: "8", label: "Liguria" },
-    { value: "9", label: "Lombardia" },
-    { value: "10", label: "Marche" },
-    { value: "11", label: "Molise" },
-    { value: "12", label: "Piemonte" },
-    { value: "13", label: "Puglia" },
-    { value: "14", label: "Sardegna" },
-    { value: "15", label: "Sicilia" },
-    { value: "16", label: "Toscana" },
-    { value: "17", label: "Trentino Alto Adige" },
-    { value: "18", label: "Umbria" },
-    { value: "19", label: "Valle d'Aosta" },
-    { value: "20", label: "Veneto" },
-];
+            { value: "1", label: "Abruzzo" },
+            { value: "2", label: "Basilicata" },
+            { value: "3", label: "Calabria" },
+            { value: "4", label: "Campania" },
+            { value: "5", label: "Emilia Romagna" },
+            { value: "6", label: "Friuli Venezia Giulia" },
+            { value: "7", label: "Lazio" },
+            { value: "8", label: "Liguria" },
+            { value: "9", label: "Lombardia" },
+            { value: "10", label: "Marche" },
+            { value: "11", label: "Molise" },
+            { value: "12", label: "Piemonte" },
+            { value: "13", label: "Puglia" },
+            { value: "14", label: "Sardegna" },
+            { value: "15", label: "Sicilia" },
+            { value: "16", label: "Toscana" },
+            { value: "17", label: "Trentino Alto Adige" },
+            { value: "18", label: "Umbria" },
+            { value: "19", label: "Valle d'Aosta" },
+            { value: "20", label: "Veneto" },
+        ];
         const suggest = (query: any, populateResults: any) => {
             const filteredResults = multiOptions.filter((i) => i.label.toLowerCase().includes(query.toLowerCase()));
             let data = filteredResults.map((item) => {
@@ -212,14 +216,14 @@ export const ReadonlyNormalizzato: Story = {
         return (
             <FormGroup className="select-wrapper">
                 <label htmlFor="autocomplete">Provincia</label>
-                {                 <Autocomplete
+                <Autocomplete
                     id="autocomplete"
                     source={suggest}
                     placeholder={"Testo da cercare"}
                     defaultValue={""}
                     displayMenu={"inline"}
                     tNoResults={() => "Nessun risultato"}
-                /> }
+                />
             </FormGroup>
         );
     },
@@ -228,7 +232,7 @@ export const ReadonlyNormalizzato: Story = {
             canvas: { sourceState: "none" },
         },
     },
-}; */
+};
 
 export const AreaDiTesto: Story = { render: () => <TextArea label="Esempio di area di testo" rows={3} /> }
 
@@ -238,49 +242,57 @@ export const AreaDiTestoConSegnaposto: Story = {
     )
 };
 
+const InputDatepickerHooks = () => {
+    const [value, setValue] = useState("");
+
+    return (
+        <Input
+            type="date"
+            label="Datepicker"
+            className="active"
+            placeholder="22/12/2023"
+            value={value}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
+};
+
 export const InputDatepicker: Story = {
     render: () => {
-        const [value, setValue] = useState("");
-
-        return (
-            <Input
-                type="date"
-                label="Datepicker"
-                className="active"
-                placeholder="22/12/2023"
-                value={value}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputDatepickerHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
+}
+
+const InputHourpickerHooks = () => {
+    const [value, setValue] = useState("");
+
+    return (
+        <Input
+            type="time"
+            label="Hourpicker"
+            className="active"
+            value={value}
+            onChange={(ev) => {
+                setValue(ev.target.value);
+            }}
+        />
+    );
 };
 
 export const InputHourpicker: Story = {
     render: () => {
-        const [value, setValue] = useState("");
-
-        return (
-            <Input
-                type="time"
-                label="Hourpicker"
-                className="active"
-                value={value}
-                onChange={(ev) => {
-                    setValue(ev.target.value);
-                }}
-            />
-        );
+        return <InputHourpickerHooks />
     },
     parameters: {
         docs: {
             canvas: { sourceState: "none" },
         },
     },
-};
+}
