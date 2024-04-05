31f4d7e5e8608c0c5d6ef21a41a36663099b938a
Virtute90 <matteo.ave@gmail.com>
2024-04-05 22:01:12 +0000

    fix: back to one snap file

--- test/Storybook.test.tsx
+++ test/Storybook.test.tsx
@@ -1,9 +1,8 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from '@jest/globals';
 import { composeStories, type Meta, type StoryFn } from '@storybook/react';
 import { act, render } from '@testing-library/react';
-import * as glob from "glob";
-import "jest-specific-snapshot";
-import path from "path";
+import * as glob from 'glob';
+import path from 'path';
 import './__mocks__/IntersectionObserverMock.ts';
 
 type StoryFile = {
@@ -11,14 +10,12 @@ type StoryFile = {
   [name: string]: StoryFn | Meta;
 };
 
-const compose = (
-  entry: StoryFile
-): ReturnType<typeof composeStories<StoryFile>> => {
+const compose = (entry: StoryFile): ReturnType<typeof composeStories<StoryFile>> => {
   try {
     return composeStories(entry);
   } catch (e) {
     throw new Error(
-      `There was an issue composing stories for the module: ${JSON.stringify(entry)}, ${e}`
+      `There was an issue composing stories for the module: ${JSON.stringify(entry)}, ${e}`,
     );
   }
 };
@@ -32,43 +29,36 @@ function getAllStoryFiles() {
   return storyFiles.map((filePath) => {
     const storyFile = require(filePath);
     const storyDir = path.dirname(filePath);
-    const componentName = path
-      .basename(filePath)
-      .replace(/\.(stories|story)\.[^/.]+$/, "");
+    const componentName = path.basename(filePath).replace(/\.(stories|story)\.[^/.]+$/, '');
 
     return { filePath, storyFile, storyDir, componentName };
   });
 }
 
-describe("Stories Snapshots", () => {
+describe('Stories Snapshots', () => {
   getAllStoryFiles().forEach(({ storyFile, componentName }) => {
     const meta = storyFile.default;
     const title = meta.title || componentName;
 
     describe(title, () => {
-      const stories = Object.entries(compose(storyFile)).map(
-        ([name, story]) => ({ name, story })
-      );
+      const stories = Object.entries(compose(storyFile)).map(([name, story]) => ({ name, story }));
 
       if (stories.length <= 0) {
         throw new Error(
-          `No stories found for this module: ${title}. Make sure there is at least one valid story for this module.`
+          `No stories found for this module: ${title}. Make sure there is at least one valid story for this module.`,
         );
       }
 
       stories.forEach(({ name, story }) => {
         test(name, async () => {
-          const mounted = render(story())
+          const mounted = render(story());
           await act(async () => {
             // Ensures a consistent snapshot by waiting for the component to render by adding a delay of 1 ms before taking the snapshot.
             await new Promise((resolve) => setTimeout(resolve, 1));
-          });
-          // Defines the custom snapshot path location and file name
-          const customSnaphotPath = `./__snapshots__/${componentName}.test.ts.snap`;
-          //@ts-ignore
-          expect(mounted.container).toMatchSpecificSnapshot(customSnaphotPath);
+          })
+          expect(mounted.container).toMatchSnapshot();
         });
       });
     });
-  })
+  });
 });
