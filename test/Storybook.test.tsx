fc3550261639f5b6d966b6f491bd1d1c05df1bc0
Virtute90 <matteo.ave@gmail.com>
2024-04-05 21:48:12 +0000

    fix: split snap file and add act() for resolve same console.error on icon

--- test/Storybook.test.tsx
+++ test/Storybook.test.tsx
@@ -1,34 +1,24 @@
-import { icons, preloadIcons } from '../src/index';
-
+import { describe, expect, test } from "@jest/globals";
+import { composeStories, type Meta, type StoryFn } from '@storybook/react';
+import { act, render } from '@testing-library/react';
+import * as glob from "glob";
+import "jest-specific-snapshot";
+import path from "path";
 import './__mocks__/IntersectionObserverMock.ts';
 
-import * as glob from 'glob';
-
-// Replace your-framework with one of the supported Storybook frameworks (react, vue3)
-import type { Meta, StoryFn } from '@storybook/react';
-
-import { describe, expect, test } from '@jest/globals';
-
-// Replace your-testing-library with one of the supported testing libraries (e.g., react, vue)
-import { render } from '@testing-library/react';
-
-// Adjust the import based on the supported framework or Storybook's testing libraries (e.g., react, testing-vue3)
-import { composeStories } from '@storybook/react';
-import path from 'path';
-
-// Preload all the icons
-beforeAll(() => preloadIcons(icons));
 type StoryFile = {
   default: Meta;
   [name: string]: StoryFn | Meta;
 };
 
-const compose = (entry: StoryFile): ReturnType<typeof composeStories<StoryFile>> => {
+const compose = (
+  entry: StoryFile
+): ReturnType<typeof composeStories<StoryFile>> => {
   try {
     return composeStories(entry);
   } catch (e) {
     throw new Error(
-      `There was an issue composing stories for the module: ${JSON.stringify(entry)}, ${e}`,
+      `There was an issue composing stories for the module: ${JSON.stringify(entry)}, ${e}`
     );
   }
 };
@@ -36,59 +26,49 @@ const compose = (entry: StoryFile): ReturnType<typeof composeStories<StoryFile>>
 function getAllStoryFiles() {
   // Place the glob you want to match your stories files
   const storyFiles = glob.sync(
-    path.join(__dirname, '..', 'stories/**/*.{stories,story}.{js,jsx,mjs,ts,tsx}')
+    path.join(__dirname, '..', 'stories/**/*.{stories,story}.{js,jsx,mjs,ts,tsx}'),
   );
 
   return storyFiles.map((filePath) => {
     const storyFile = require(filePath);
-    return { filePath, storyFile };
+    const storyDir = path.dirname(filePath);
+    const componentName = path
+      .basename(filePath)
+      .replace(/\.(stories|story)\.[^/.]+$/, "");
+
+    return { filePath, storyFile, storyDir, componentName };
   });
 }
 
-// Recreate similar options to Storyshots. Place your configuration below
-const options = {
-  suite: 'Storybook Tests',
-  storyKindRegex: /^.*?DontTest$/,
-  storyNameRegex: /UNSET/,
-  snapshotsDirName: '__snapshots__',
-  snapshotExtension: '.storyshot',
-};
-
-describe(options.suite, () => {
-  getAllStoryFiles().forEach(({ storyFile, filePath }) => {
+describe("Stories Snapshots", () => {
+  getAllStoryFiles().forEach(({ storyFile, componentName }) => {
     const meta = storyFile.default;
-    const title = meta.title || filePath;
-
-    if (options.storyKindRegex.test(title) || meta.parameters?.storyshots?.disable) {
-      // Skip component tests if they are disabled
-      return;
-    }
+    const title = meta.title || componentName;
 
     describe(title, () => {
-      const stories = Object.entries(compose(storyFile))
-        .map(([name, story]) => ({ name, story }))
-        .filter(({ name, story }) => {
-          // Implements a filtering mechanism to avoid running stories that are disabled via parameters or that match a specific regex mirroring the default behavior of Storyshots.
-          return !options.storyNameRegex.test(name) && !story.parameters.storyshots?.disable;
-        });
+      const stories = Object.entries(compose(storyFile)).map(
+        ([name, story]) => ({ name, story })
+      );
 
       if (stories.length <= 0) {
         throw new Error(
-          `No stories found for this module: ${title}. Make sure there is at least one valid story for this module, without a disable parameter, or add parameters.storyshots.disable in the default export of this file.`,
+          `No stories found for this module: ${title}. Make sure there is at least one valid story for this module.`
         );
       }
 
       stories.forEach(({ name, story }) => {
-        // Instead of not running the test, you can create logic to skip it, flagging it accordingly in the test results.
-        const testFn = story.parameters.storyshots?.skip ? test.skip : test;
-
-        testFn(name, async () => {
-          const mounted = render(story());
-          // Ensures a consistent snapshot by waiting for the component to render by adding a delay of 1 ms before taking the snapshot.
-          await new Promise((resolve) => setTimeout(resolve, 1));
-          expect(mounted.container).toMatchSnapshot();
+        test(name, async () => {
+          const mounted = render(story())
+          await act(async () => {
+            // Ensures a consistent snapshot by waiting for the component to render by adding a delay of 1 ms before taking the snapshot.
+            await new Promise((resolve) => setTimeout(resolve, 1));
+          });
+          // Defines the custom snapshot path location and file name
+          const customSnaphotPath = `./__snapshots__/${componentName}.test.ts.snap`;
+          //@ts-ignore
+          expect(mounted.container).toMatchSpecificSnapshot(customSnaphotPath);
         });
       });
     });
-  });
+  })
 });
