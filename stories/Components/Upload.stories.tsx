9cd0207128f9bb9958eb7cbd35e653f57e6c7f38
Virtute90 <matteo.ave@gmail.com>
2024-05-27 16:41:22 +0200

    feat: upload con avatar ok

--- stories/Components/Upload.stories.tsx
+++ stories/Components/Upload.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { Upload, UploadList, UploadListItem } from '../../src';
+import { Col, Container, Row, Upload, UploadList, UploadListItem } from '../../src';
 import { UploadListItemProps } from '../../src/Upload/UploadListItem';
 
 const meta: Meta<typeof Upload> = {
@@ -13,7 +13,7 @@ export default meta;
 type Story = StoryObj<typeof Upload>;
 
 interface listaFilesArgsI {
-  [type: string]: UploadListItemProps
+  [type: string]: UploadListItemProps;
 }
 
 export const EsempioInput: Story = {
@@ -41,15 +41,15 @@ const listaFilesArgs: listaFilesArgsI = {
   },
   error: {
     uploadStatus: 'error',
-    fileName: 'nome-file-03.pdf',
+    fileName: 'nome-file-03.pdf'
   }
 };
 
 export const ListaFiles = {
   render: ({ ...listaFilesArgs }: listaFilesArgsI) => (
     <UploadList>
-      {Object.values(listaFilesArgs).map(type => {
-        return <UploadListItem key={type.fileName} {...type} />
+      {Object.values(listaFilesArgs).map((type) => {
+        return <UploadListItem key={type.fileName} {...type} />;
       })}
     </UploadList>
   ),
@@ -88,27 +88,48 @@ const listaFilesImmagineArgs: listaFilesArgsI = {
     previewImageSrc: 'https://picsum.photos/40/40?image=1058',
     previewImageAlt: 'descrizione immagine',
     uploadStatus: 'error',
-    fileName: 'nome-file-03.pdf',
+    fileName: 'nome-file-03.pdf'
   }
 };
 
 export const ListaFilesImmagine = {
   render: ({ ...listaFilesImmagineArgs }: listaFilesArgsI) => (
     <UploadList previewImage>
-      {Object.values(listaFilesImmagineArgs).map(type => {
-        return <UploadListItem key={type.fileName} {...type} />
+      {Object.values(listaFilesImmagineArgs).map((type) => {
+        return <UploadListItem key={type.fileName} {...type} />;
       })}
     </UploadList>
   ),
   args: {
-    ...listaFilesImmagineArgs,
+    ...listaFilesImmagineArgs
   }
 };
 
 export const UploadAvatar: Story = {
-  render: (args) => <Upload {...args} />,
+  render: (args) => (
+    <Container>
+      <Row>
+        <Col>
+          <Upload
+            {...args}
+            id='ExampleUpload3'
+            avatarImg={<img src='https://randomuser.me/api/portraits/men/21.jpg' alt='descrizione immagine' />}
+          />
+        </Col>
+        <Col>
+          <Upload
+            {...args}
+            id='ExampleUpload4'
+            avatarImg={<img src='https://randomuser.me/api/portraits/women/21.jpg' alt='descrizione immagine' />}
+            avatarSmall
+          />
+        </Col>
+      </Row>
+    </Container>
+  ),
   args: {
-    id: 'ExampleUpload',
+    label: 'Aggiorna',
+    icon: 'it-camera',
     isAvatar: true
   }
 };
