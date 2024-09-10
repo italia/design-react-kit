548608a15b4a55061e9f2469ea3a5e79fefc4d3a
Virtute90 <32056724+Virtute90@users.noreply.github.com>
2024-09-10 16:51:10 +0200

    feat: improve dimmer component

--- src/Dimmer/Dimmer.tsx
+++ src/Dimmer/Dimmer.tsx
@@ -1,15 +1,17 @@
-import React, { FC, HTMLAttributes } from 'react';
 import classNames from 'classnames';
+import React, { FC, HTMLAttributes } from 'react';
 
 import { Icon } from '../Icon/Icon';
 
 export interface DimmerProps extends HTMLAttributes<HTMLElement> {
-  /** Il nome dell'icona da mostrare. Per una lista completa vedi: @TODO-URL */
+  /** Il nome dell'icona da mostrare */
   icon?: string;
   /** Le varianti di colore definite in Bootstrap Italia */
-  color?: 'success' | 'warning' | 'danger' | 'note' | 'important' | string;
+  color?: 'primary' | 'secondary';
   /** Classi aggiuntive da usare per il componente Dimmer */
   className?: string;
+  /** Mostra il wrapper */
+  show?: boolean;
   /**
    * Classi aggiuntive da usare per il componente contenitore del Dimmer
    * Per replicare il comportamento precedente, in cui `className` veniva applicato anche al wrapper,
@@ -19,20 +21,28 @@ export interface DimmerProps extends HTMLAttributes<HTMLElement> {
   testId?: string;
 }
 
-export const Dimmer: FC<DimmerProps> = ({ icon, color, className, wrapperClassName, testId, ...attributes }) => {
-  const { children, ...rest } = attributes;
-  const classes = classNames('dimmer', wrapperClassName === true ? className : wrapperClassName, {
-    [`dimmer-${color}`]: color
-  });
-  const innerClasses = classNames('dimmer-inner', className);
-  const dimmerIcon = icon && (
-    <div className='dimmer-icon'>
-      <Icon icon={icon} />
-    </div>
-  );
+export const Dimmer: FC<DimmerProps> = ({
+  icon,
+  color,
+  className,
+  show = true,
+  wrapperClassName,
+  testId,
+  ...attributes
+}) => {
+  const { children, ...rest } = attributes,
+    classes = classNames('dimmer fade', { show: show }, wrapperClassName === true ? className : wrapperClassName, {
+      [`dimmer-${color}`]: color
+    }),
+    innerClasses = classNames('dimmer-inner', className),
+    dimmerIcon = icon && (
+      <div className='dimmer-icon'>
+        <Icon icon={icon} />
+      </div>
+    );
 
   return (
-    <div className={classes} {...rest} style={{ display: 'flex' }} data-testid={testId}>
+    <div className={classes} {...rest} data-testid={testId} aria-hidden={show ? undefined : true}>
       <div className={innerClasses} {...rest}>
         {dimmerIcon}
         {children}
