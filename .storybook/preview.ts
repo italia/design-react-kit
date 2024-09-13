272ff3cd1cb02b259c0a69f0afb09311f42cf1b4
Virtute90 <matteo.ave@gmail.com>
2024-09-13 10:57:00 +0200

    feat: aggiunti stili per callouts per doc come in BSI

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
 import { Preview } from "@storybook/react";
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
-import "../assets/css/bi-fonts.scss";
 import "../assets/css/storybook-fixes.css";
+import "../assets/docs/scss/bi-fonts.scss";
+import "../assets/docs/scss/docs.scss";
+
 import theme from "./theme";
 
 const preview: Preview = {
