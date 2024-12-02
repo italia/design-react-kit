f8ae2b1d2d3264d82bbfd8167d2ca3a862bfef98
Andrea Stagi <stagi.andrea@gmail.com>
2024-12-02 16:17:23 +0100

    chore: tentative to fix tree shake

--- src/Dropdown/DropdownMenu.tsx
+++ src/Dropdown/DropdownMenu.tsx
@@ -2,7 +2,9 @@ import React, { FC, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { DropdownMenu as BSDRopdownMenu } from 'reactstrap';
 
-import './dropdown.css'
+// .it-header-slim-wrapper .it-header-slim-wrapper-content .dropdown-menu {
+//   top: unset !important;
+// }
 
 export interface DropdownMenuProps extends HTMLAttributes<HTMLElement> {
   /** Classi aggiuntive da usare per il componente Button */
@@ -14,9 +16,9 @@ export const DropdownMenu: FC<DropdownMenuProps> = ({ className, testId, childre
   const classes = classNames(className, {
     'dropdown-menu': true
   });
-
+  
   return (
-    <BSDRopdownMenu className={classes} data-testid={testId} {...attributes}>
+    <BSDRopdownMenu  className={classes} data-testid={testId} {...attributes}>
       {children}
     </BSDRopdownMenu>
   );
