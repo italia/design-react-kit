5dcd77dd4ef65a9479e7a1167e67eb338287333d
Lorezz <point.point@gmail.com>
2025-03-05 10:08:12 +0100

    wip youtube video, and disclaimer

--- src/Video/Video.tsx
+++ src/Video/Video.tsx
@@ -1,13 +1,33 @@
-import React, { ElementType, FC, ReactNode, useEffect } from 'react';
-
-import { CSSModule } from 'reactstrap/types/lib/utils';
-import { InputProps } from '../Input/Input';
+import React, { ElementType, FC, useEffect } from 'react';
 
 import { VideoPlayer } from 'bootstrap-italia';
+import { CSSModule } from 'reactstrap/types/lib/utils';
+import { Input, InputProps } from '../Input/Input';
+import { Accordion } from '../Accordion/Accordion';
+import { AccordionItem } from '../Accordion/AccordionItem';
+import { AccordionHeader } from '../Accordion/AccordionHeader';
+import { AccordionBody } from '../Accordion/AccordionBody';
+import { Dimmer } from '../Dimmer/Dimmer';
+import { DimmerButtons } from '../Dimmer/DimmerButtons';
+import { Button } from '../Button/Button';
+import { FormGroup, Label } from 'reactstrap';
 
+export interface VideoSource {
+  src: string;
+  type: string;
+}
+export interface TrackSource {
+  kind: string;
+  src: string;
+  srcLang: string;
+  label: string;
+  isDefault?: boolean;
+}
+export interface transcription {
+  src: string;
+  type: 'video/mp4' | 'video/webm' | 'video/ogg';
+}
 export interface VideoProps extends InputProps {
-  /** Label da mostrare per il componente */
-  label: string | ReactNode;
   /** Dimensioni ammissibili per il componente */
   bsSize?: 'lg' | 'sm';
   /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
@@ -16,49 +36,108 @@ export interface VideoProps extends InputProps {
   innerRef?: React.Ref<HTMLInputElement>;
   /** Oggetto contenente la nuova mappatura per le classi CSS. */
   cssModule?: CSSModule;
-  testId?: string;
+  sources?: Array<VideoSource>;
+  transcription?: string;
+  transcriptionLabel?: string;
+  tracks?: Array<TrackSource>;
+
+  poster?: string;
+  controls?: boolean;
+  autoplay?: boolean;
+  loop?: boolean;
+  fluid?: boolean;
+
+  showDisclaimer?: boolean;
+  youtubeUrl?: string;
 }
 
-export const Video: FC<VideoProps> = ({ label, testId }) => {
+export const Video: FC<VideoProps> = (props) => {
   let vpInstance: VideoPlayer;
+
   useEffect(() => {
     const el = document.querySelector('video');
     if (el && VideoPlayer) {
       vpInstance = new VideoPlayer(el);
-      setTimeout(() => {
-        console.log(vpInstance.player.log); // Con .player puoi usare play(), stop() ecc ..
-      }, 3000);
+      // setTimeout(() => {
+      //   console.log(vpInstance.player.log); // Con .player puoi usare play(), stop() ecc ..
+      // }, 3000);
+      if (props.youtubeUrl) {
+        loadYouTubeVideo(props.youtubeUrl, vpInstance);
+      }
+
+      if (props.autoplay) {
+        setTimeout(() => {
+          vpInstance?.player?.play();
+        }, 1000);
+      }
     }
-  });
+    return () => {
+      if (vpInstance) {
+        vpInstance.dispose();
+      }
+    };
+  }, []);
+
+  function loadYouTubeVideo(url: string, vpInstance: VideoPlayer) {
+    if (vpInstance) {
+      vpInstance.setYouTubeVideo(url);
+    }
+  }
+
+  const { controls = true, autoplay = false, loop = false, fluid = true, poster = undefined } = props;
+  const videoProps = { controls, autoplay, loop, fluid, poster };
+
+  const [show, setShow] = React.useState(false);
+
   return (
-    <div className='row' data-testid={testId}>
-      <video controls data-bs-video>
-        <source src='//vjs.zencdn.net/v/oceans.mp4' type='video/mp4' />
-        <source src='//vjs.zencdn.net/v/oceans.webm' type='video/webm' />
-      </video>
-      <div className='vjs-transcription accordion'>
-        <div className='accordion-item'>
-          <h2 className='accordion-header ' id='transcription-head1'>
-            <button
-              className='accordion-button collapsed'
-              type='button'
-              data-bs-target='#transcription1'
-              aria-expanded='true'
-              aria-controls='transcription'
-            >
-              {label}
-            </button>
-          </h2>
-          <div
-            id='transcription1'
-            className='accordion-collapse collapse'
-            role='region'
-            aria-labelledby='transcription-head1'
-          >
-            <div className='accordion-body'>{label}</div>
-          </div>
-        </div>
+    <>
+      <div className='row dimmable'>
+        <video {...videoProps} preload='auto' className='video-js' data-bs-video={true}>
+          {props.sources?.map((source) => <source key={source.src} src={source.src} type={source.type} />)}
+          {props.tracks?.map((track) => {
+            const { src, kind, label, srcLang, isDefault } = track;
+            return (
+              <track
+                key={src}
+                kind={kind}
+                src={src}
+                srcLang={srcLang || ''}
+                label={label}
+                default={isDefault || false}
+              />
+            );
+          })}
+        </video>
+        {props.transcription && (
+          <Accordion className='vjs-transcription'>
+            <AccordionItem>
+              <AccordionHeader active={show} onToggle={() => setShow((p) => !p)}>
+                {props.transcriptionLabel || 'Trascrizione'}
+              </AccordionHeader>
+              <AccordionBody active={show}>{props.transcription}</AccordionBody>
+            </AccordionItem>
+          </Accordion>
+        )}
       </div>
-    </div>
+      <Dimmer icon='it-unlocked'>
+        <p>Accetta i cookie di YouTube per vedere il video. Puoi gestire le preferenze nella cookie policy.</p>
+        <DimmerButtons>
+          <Button
+            onClick={() => {
+              console.log('click');
+            }}
+            color='primary'
+          >
+            Accetta
+          </Button>
+          <FormGroup check inline>
+            <Input id='inline-checkbox' type='checkbox' />
+            <Label check for='inline-checkbox' defaultChecked={false}>
+              Ricorda per tutti i video
+            </Label>
+          </FormGroup>
+        </DimmerButtons>
+      </Dimmer>
+    </>
   );
 };
--- stories/Components/Video.stories.tsx
+++ stories/Components/Video.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { Video } from '../../src';
+import type { TrackSource } from '../../src/Video/Video';
 
 const meta: Meta<typeof Video> = {
   title: 'Documentazione/Componenti/Video',
@@ -16,15 +17,128 @@ export default meta;
 
 type Story = StoryObj<typeof Video>;
 
-const EsempiVideo = () => {
-  return (
-    <div className='bd-example tooltip-demo'>
-      <Video label="boh"></Video>
-    </div>
-  );
+export const Trascription: Story = {
+  render: () => {
+    const sources = [
+      { src: '//vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4' },
+      { src: '//vjs.zencdn.net/v/oceans.webm', type: 'video/webm' }
+    ];
+    const transcription = 'Questa è la trascrizione testuale del video';
+    return (
+      <div>
+        <div>Video</div>
+        <Video
+          sources={sources}
+          transcriptionLabel={'Mostra la trascrizione del video'}
+          transcription={transcription}
+        />
+        <p>In questo esempio viene passato del testo relativo alla trascrizione video</p>
+      </div>
+    );
+  }
 };
+
+export const Tracks: Story = {
+  render: () => {
+    const tracks: TrackSource[] = [
+      {
+        src: 'https://italia.github.io/bootstrap-italia/docs/assets/video/subtitles-it.vtt',
+        kind: 'subtitles',
+        label: 'Italiano',
+        srcLang: 'it',
+        isDefault: true
+      },
+      {
+        src: 'https://italia.github.io/bootstrap-italia/docs/assets/video/subtitles-en.vtt',
+        kind: 'subtitles',
+        label: 'English',
+        srcLang: 'en',
+        isDefault: false
+      },
+      {
+        kind: 'captions',
+        src: 'https://italia.github.io/bootstrap-italia/docs/assets/video/subtitles-es.vtt',
+        srcLang: 'es',
+        label: 'Español',
+        isDefault: false
+      }
+    ];
+    const sources = [
+      {
+        src: 'https://italia.github.io/bootstrap-italia/docs/assets/video/ElephantsDreamDASH/ElephantsDream.mp4.mpd',
+        type: 'application/dash+xml'
+      }
+    ];
+
+    const poster = 'https://italia.github.io/bootstrap-italia/docs/assets/video/ElephantsDream.mp4-poster21.jpg';
+    return (
+      <div>
+        <div>Video</div>
+        <Video sources={sources} tracks={tracks} poster={poster} />
+        <p>
+          In questo esempio vengono passate diverse tracce per i sottotitoli: in italiano (predefinito) , inglese e
+          spagnolo
+        </p>
+      </div>
+    );
+  }
+};
+
+export const AutoplayAndControls: Story = {
+  render: () => {
+    const sources = [
+      { src: '//vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4' },
+      { src: '//vjs.zencdn.net/v/oceans.webm', type: 'video/webm' }
+    ];
+
+    return (
+      <div>
+        <div>Video</div>
+        <Video sources={sources} autoplay={true} fluid={true} controls={true} loop={true} />
+        <p>
+          In questo esempio vengono impostati seguenti parametri:
+          <ul>
+            <li>autoplay: true</li>
+            <li>fluid: true</li>
+            <li>controls: true</li>
+            <li>loop: true</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+};
+
+export const YouTubeVideo: Story = {
+  render: () => {
+    return (
+      <div>
+        <div>Video</div>
+        <Video youtubeUrl={'https://youtu.be/_0j7ZQ67KtY'} />
+        <p>In questo esempio viene passato nella prorietà 'youtubeUrl' un url di un video di YouTube</p>
+      </div>
+    );
+  }
+};
+
+//https://youtu.be/_0j7ZQ67KtY
 
 export const Esempi: Story = {
-  render: () => <EsempiVideo />
-};
+  render: () => {
+    const sources = [
+      { src: '//vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4' },
+      { src: '//vjs.zencdn.net/v/oceans.webm', type: 'video/webm' }
+    ];
 
+    return (
+      <div>
+        <div>Video</div>
+        <Video sources={sources} />
+        <p>
+          In questo esempio vengono passati al compomente solamente la prorietà relativa all'array delle sorgenti,
+          contente per ogni sorgente l'url della sorgente e sua tipologia
+        </p>
+      </div>
+    );
+  }
+};
