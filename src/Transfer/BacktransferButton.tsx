0f408a9fccea7de43f80dfb0c2a0d7738727d009
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-09 17:55:19 +0100

    feat: add backtransfer button

--- src/Transfer/BacktransferButton.tsx
+++ src/Transfer/BacktransferButton.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useTransferContext } from './useTransferContext';
 
 const BacktransferButton = () => {
-  return <>BacktransferButton</>;
+  const { targetCandidates, sourceItems, targetItems, setSourceItems, setTargetItems, setTargetCandidates } =
+    useTransferContext();
+  const handleTransfer = () => {
+    const newTargetItems = targetItems.filter((item) => !targetCandidates.includes(item.id));
+    const newSourceItems = sourceItems.concat(targetItems.filter((item) => targetCandidates.includes(item.id)));
+    setSourceItems(newSourceItems);
+    setTargetItems(newTargetItems);
+    setTargetCandidates([]);
+  };
+  return <button onClick={handleTransfer}>backtransfer </button>;
 };
 
 export { BacktransferButton };
