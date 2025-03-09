1589b8ebd968045624041a519ba89c2efab8fa60
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-09 17:55:52 +0100

    feat: add reset button

--- src/Transfer/ResetButton.tsx
+++ src/Transfer/ResetButton.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useTransferContext } from './useTransferContext';
 
 const ResetButton = () => {
-  return <>ResetButton</>;
+  const {
+    originalSourceItems,
+    originalTargetItems,
+    setSourceItems,
+    setTargetItems,
+    setSourceCandidates,
+    setTargetCandidates
+  } = useTransferContext();
+  const handleReset = () => {
+    setSourceItems(originalSourceItems);
+    setTargetItems(originalTargetItems);
+    setSourceCandidates([]);
+    setTargetCandidates([]);
+  };
+  return <button onClick={handleReset}>reset </button>;
 };
 
 export { ResetButton };
