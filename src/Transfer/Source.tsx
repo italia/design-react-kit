105798aebcb10049aac586a0e4cb7dcb2c7d2b08
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-09 17:51:01 +0100

    feat: add source and target for tranfer component

--- src/Transfer/Source.tsx
+++ src/Transfer/Source.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { InternalItem, Item } from './Item';
+import { useTransferContext } from './useTransferContext';
 
-const Source = ({ children }: { children: React.ReactNode }) => {
-  return <>Source {children}</>;
+const SourceItem = ({ id, children }: { id: string; children: React.ReactNode }) => {
+  const { sourceCandidates, setSourceCandidates } = useTransferContext();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSourceCandidates([...new Set([...sourceCandidates, e.target.id])]);
+    } else {
+      setSourceCandidates(sourceCandidates.filter((item) => item !== e.target.id));
+    }
+  };
+  const isChecked = sourceCandidates.includes(id);
+  return (
+    <InternalItem checked={isChecked} id={id} onChange={handleChange}>
+      {children}
+    </InternalItem>
+  );
 };
 
+const Source: React.FC<{ children?: React.ReactNode }> & {
+  Header: React.FC;
+} = ({ children }) => {
+  const { sourceItems, setSourceItems, setOriginalSourceItems } = useTransferContext();
+  const otherComponents: React.ReactNode[] = [];
+  const childrenSource = React.Children.map(children, (child) => {
+    if (React.isValidElement(child) && child.type === Item) {
+      const { id, children } = child.props;
+      return { id, content: children };
+    }
+    otherComponents.push(child);
+    return null;
+  })?.filter((item) => item !== null);
+
+  useEffect(() => {
+    if (!childrenSource) return;
+    setSourceItems(childrenSource);
+    setOriginalSourceItems(childrenSource);
+  }, []);
+
+  return (
+    <div>
+      {otherComponents}
+      {sourceItems.map(({ id, content }) => (
+        <SourceItem key={id} id={id}>
+          {content}
+        </SourceItem>
+      ))}
+
+      <br />
+    </div>
+  );
+};
 Source.Header = () => {
   return <>Source.Header</>;
 };
--- src/Transfer/Target.tsx
+++ src/Transfer/Target.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { InternalItem, Item } from './Item';
+import { useTransferContext } from './useTransferContext';
 
-const Target = ({ children }: { children: React.ReactNode }) => {
-  return <>Target {children}</>;
+const TargetItem = ({ id, children }: { id: string; children: React.ReactNode }) => {
+  const { targetCandidates, setTargetCandidates } = useTransferContext();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setTargetCandidates([...new Set([...targetCandidates, e.target.id])]);
+    } else {
+      setTargetCandidates(targetCandidates.filter((item) => item !== e.target.id));
+    }
+  };
+  const isChecked = targetCandidates.includes(id);
+  return (
+    <InternalItem checked={isChecked} id={id} onChange={handleChange}>
+      {children}
+    </InternalItem>
+  );
 };
 
+const Target: React.FC<{ children?: React.ReactNode }> & {
+  Header: React.FC;
+} = ({ children }) => {
+  const { targetItems, setTargetItems, setOriginalTargetItems } = useTransferContext();
+  const otherComponents: React.ReactNode[] = [];
+  const childrenTarget = React.Children.map(children, (child) => {
+    if (React.isValidElement(child) && child.type === Item) {
+      const { id, children } = child.props;
+      return { id, content: children };
+    }
+    otherComponents.push(child);
+    return null;
+  })?.filter((item) => item !== null);
+
+  useEffect(() => {
+    if (!childrenTarget) return;
+    setTargetItems(childrenTarget);
+    setOriginalTargetItems(childrenTarget);
+  }, []);
+
+  return (
+    <div>
+      {otherComponents}
+      {targetItems.map(({ id, content }) => (
+        <TargetItem key={id} id={id}>
+          {content}
+        </TargetItem>
+      ))}
+    </div>
+  );
+};
 Target.Header = () => {
   return <>Target.Header</>;
 };
