b5b0fd8a26e8ef7d13bd235c7171e1d649c89bf1
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2025-06-24 10:13:34 +0200

    fix(build): asset drag n drop icon

--- src/Upload/UploadDragNdrop.tsx
+++ src/Upload/UploadDragNdrop.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React, { DragEventHandler, FC, useState } from 'react';
 import { Icon } from '../Icon/Icon';
-import DragandDropIcon from '../assets/upload-drag-drop-icon.svg';
+import DragandDropIcon from '../../assets/images/upload-drag-drop-icon.svg';
 
 interface UploadDragNdropProps {
   /**
