0c46028e660f826880d887dfae05f3aef9815d9f
Andrea Stagi <stagi.andrea@gmail.com>
2025-09-16 17:22:51 +0200

    feat: add dimmer stories

--- stories/Components/Dimmer.stories.tsx
+++ stories/Components/Dimmer.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react-vite';
 import React from 'react';
-import { Button, Card, CardBody, CardText, CardTitle, Dimmer, DimmerButtons } from '../../src';
+import { Button, Card, Dimmer, DimmerButtons } from '../../src';
 
 const colors = ['primary', 'secondary'];
 
@@ -26,47 +26,68 @@ export const Esempi: Story = {
       <div className='row dimmable'>
         <div className='col-12 col-lg-4'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
 
         <div className='col-12 col-lg-4 d-none d-lg-block'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
         <div className='col-12 col-lg-4 d-none d-lg-block'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
         </div>
       </div>
@@ -100,34 +121,48 @@ export const Esempi: Story = {
 export const DimmerConAzioni: Story = {
   render: () => (
     <div>
-      <div className='col dimmable'>
-        <div className='col-12 col-lg-4'>
+      <div className='row dimmable'>
+        <div className='col-12 col-md-6 col-lg-6 my-3 my-md-4'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
-        <div className='col-12 col-lg-4 d-none d-lg-block'>
+        <div className='col-12 col-md-6 col-lg-6 my-3 my-md-4'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
@@ -148,34 +183,48 @@ export const DimmerConAzioni: Story = {
 export const DimmerConAzioniColorePrimario: Story = {
   render: () => (
     <div>
-      <div className='col dimmable'>
-        <div className='col-12 col-lg-4'>
+      <div className='row dimmable'>
+        <div className='col-12 col-md-6 col-lg-6 my-3 my-md-4'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
-        <div className='col-12 col-lg-4 d-none d-lg-block'>
+        <div className='col-12 col-md-6 col-lg-6 my-3 my-md-4'>
           {/* start card */}
-          <Card>
-            <CardBody>
-              <CardTitle tag='h5'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-              </CardText>
-            </CardBody>
+          <Card image rounded border>
+            <h3 className="it-card-title ">
+              <a href="#">Titolo del contenuto</a>
+            </h3>
+            <div className="it-card-image-wrapper">
+              <div className="ratio ratio-16x9">
+                <figure className="figure img-full">
+                  <img src="https://placeholderimage.eu/api/city/800/600" alt="Breve descrizione immagine se ha senso nel contesto, marcare altrimenti come decorativa lasciando l'alt applicato ma vuoto."></img>
+                </figure>
+              </div>
+            </div>
+            <div className="it-card-body">
+              <p className="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione in massimo tre o quattro righe, senza troncamento.</p>
+            </div>
+            <footer className="it-card-related it-card-footer">
+              <time className="it-card-date" dateTime="2025-04-22">22 aprile 2025</time>
+            </footer>
           </Card>
           {/* end card */}
         </div>
